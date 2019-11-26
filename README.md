dd3c4617f4b189f3383648335852e907a86cbb37
Zhixing <zhixing.wang@ttu.edu>
2019-11-26 06:52:18 +0100

    update

--- README.md
+++ README.md
@@ -16,6 +16,22 @@ cd $CMSSW_BASE/CMSDIJET/DijetRootTreeAnalyzer
 
 Start
 ===========
+choose the right model and then put it in to step1.py
 ```
 python step1.py
 ```
+Then excute the command printed
+
+
+choose the right model, data year and Folder produced in the last step into step2_Inter.py
+```
+python step2.py
+```
+excute step2_Inter_*.sh
+
+choose the Folder produced in the first step and put it into step3_Inter_2018_*p.py
+```
+python step3_Inter_2018_*p.py
+```
+excute aloha_0.sh first(adding all tag and uncerntainty file to the right position)
+then excute other aloha file
---
+++ step2_Inter_2.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+python python/bTag_extractShapes_Interpolater_scan_2018_Inter_up.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_up_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_up_2b
+python python/ReScaleInterpolation_2018_Inter_UD.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_up_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2018_Inter_central.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_central_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_central_2b
+python python/ReScaleInterpolation_2018_Inter.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_central_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2018_Inter_down.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_down_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_down_2b
+python python/ReScaleInterpolation_2018_Inter_UD.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2018Scan_DeepJet_down_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2016_Inter_up.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_up_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_up_2b
+python python/ReScaleInterpolation_2016_Inter_UD.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_up_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2016_Inter_central.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_central_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_central_2b
+python python/ReScaleInterpolation_2016_Inter.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_central_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2016_Inter_down.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_down_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_down_2b
+python python/ReScaleInterpolation_2016_Inter_UD.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2016Scan_DeepJet_down_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2017_Inter_up.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_up_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_up_2b
+python python/ReScaleInterpolation_2017_Inter_UD.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_up_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2017_Inter_central.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_central_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_central_2b
+python python/ReScaleInterpolation_2017_Inter.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_central_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2017_Inter_down.py -m  DMgq0p25 -e signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_down_2b -c 2b
+python python/extract.py -m  DMgq0p25  -i signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_down_2b
+python python/ReScaleInterpolation_2017_Inter_UD.py  -m  DMgq0p25  -F signalHistos_DMgq0p25_bb_NovInter_For2017Scan_DeepJet_down_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2018_Inter_up.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_up_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_up_2b
+python python/ReScaleInterpolation_2018_Inter_UD.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_up_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2018_Inter_central.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_central_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_central_2b
+python python/ReScaleInterpolation_2018_Inter.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_central_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2018_Inter_down.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_down_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_down_2b
+python python/ReScaleInterpolation_2018_Inter_UD.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2018Scan_DeepJet_down_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2016_Inter_up.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_up_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_up_2b
+python python/ReScaleInterpolation_2016_Inter_UD.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_up_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2016_Inter_central.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_central_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_central_2b
+python python/ReScaleInterpolation_2016_Inter.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_central_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2016_Inter_down.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_down_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_down_2b
+python python/ReScaleInterpolation_2016_Inter_UD.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2016Scan_DeepJet_down_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2017_Inter_up.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_up_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_up_2b
+python python/ReScaleInterpolation_2017_Inter_UD.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_up_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2017_Inter_central.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_central_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_central_2b
+python python/ReScaleInterpolation_2017_Inter.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_central_2b -c 2b
+
+python python/bTag_extractShapes_Interpolater_scan_2017_Inter_down.py -m  DMgq0p2 -e signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_down_2b -c 2b
+python python/extract.py -m  DMgq0p2  -i signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_down_2b
+python python/ReScaleInterpolation_2017_Inter_UD.py  -m  DMgq0p2  -F signalHistos_DMgq0p2_bb_NovInter_For2017Scan_DeepJet_down_2b -c 2b
+
