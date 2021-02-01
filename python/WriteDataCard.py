702c8cdbf50adcb978da2db1f8f6de835fd2db61
HsinYeh <thankyouyou06@gmail.com>
2021-02-01 11:27:12 +0800

    tmp

--- python/WriteDataCard.py
+++ python/WriteDataCard.py
@@ -595,8 +595,8 @@ if __name__ == '__main__':
         wIn = inputRootFile.Get("w"+box).Clone("wIn"+box)
         if wIn.obj("fitresult_extDijetPdf_data_obs") != None:
             frIn = wIn.obj("fitresult_extDijetPdf_data_obs")
-        elif wIn.obj("nll_extDijetPdf_%s_dijet_data_obs"% box.split("DiPhotons_")[1] ) != None:
-            frIn = wIn.obj("nll_extDijetPdf_%s_dijet_data_obs"% box.split("DiPhotons_")[1])  #FIXME I add "_dijet to the original name"
+        elif wIn.obj("nll_extDijetPdf_%s_data_obs"% box.split("DiPhotons_")[1] ) != None:
+            frIn = wIn.obj("nll_extDijetPdf_%s_data_obs"% box.split("DiPhotons_")[1])  #FIXME I add "_dijet to the original name"
         elif wIn.obj("fitresult_extDijetPdf_data_obs_with_constr") != None:
             fr = wIn.obj("fitresult_extDijetPdf_data_obs_with_constr")
         elif wIn.obj("nll_extDijetPdf_data_obs_with_constr") != None:
