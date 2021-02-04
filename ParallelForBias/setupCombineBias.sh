dd5d259a8bca8c9b32b84a8d8eb18a7078d105e2
HsinYeh <thankyouyou06@gmail.com>
2021-02-04 12:15:13 +0800

    tmp

--- ParallelForBias/setupCombineBias.sh
+++ ParallelForBias/setupCombineBias.sh
@@ -33,15 +33,19 @@ cp -r ${mainpath}/config .
 cp -r ${mainpath}/output .
 cp -r ${mainpath}/bkgAltModels .
 
+touch test.txt
+
 time python python/RunBias.py -c config/diphotons_bias_${THEYEAR}.config -i bkgAltModels/${THEMODEL}/blind/FitResults_DiPhotons_${THECOUP}_${THECAT}_${THEYEAR}.root -b DiPhotons_${THECOUP}_${THECAT} --mass ${THEMASS} -m gg -d signal_bias -r ${THEMUIN} -l ${THELUMI} --year ${THEYEAR} -t ${THENTOYS} --gen-pdf ${THEMODEL} --fit-pdf ${THENOMINALMODEL}
 
 outfile=`ls fitDiagnostics*|grep ${THECOUP}|grep ${THEMODEL}|grep ${THENOMINALMODEL}|grep .root`
 
-echo ${outfile}
+echo ${outfile} >> test.txt
 
 mv ${outfile} fitDiagnostics_${THEINSIGNAME}_mu${THEMUIN}_${THECOUP}_${THECAT}_${THEMODEL}_${THEMASS}_${CURRENTTOY}.root
 
 cp fitDiagnostics_${THEINSIGNAME}_mu${THEMUIN}_${THECOUP}_${THECAT}_${THEMODEL}_${THEMASS}_${CURRENTTOY}.root ${THEINOUTPATH}/${THECOUP}/${THECAT}/mu${THEMUIN}/${THEMODEL}/mass${THEMASS}/.
+
+echo "Finish cp" >> test.txt
 
 rm -rf *.root
 
