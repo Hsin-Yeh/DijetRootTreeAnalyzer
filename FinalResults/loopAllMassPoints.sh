e98c4cdd4fcec599d43726fab82feec25b57cdcf
HsinYeh <thankyouyou06@gmail.com>
2023-05-01 16:24:29 +0800

    debug

--- FinalResults/loopAllMassPoints.sh
+++ FinalResults/loopAllMassPoints.sh
@@ -28,7 +28,7 @@ touch ${finalResults}
 
 #a priori limits. I see in the post-fit or a-posteriori expected limit weird
 #one and two sigma region above 1.2 TeV. So, I will go to a priori limits at the moment.
-if [${combine_methods} == "AsymptoticLimits_bypassFrequentistFit"]; then
+if [[ ${combine_methods} == "AsymptoticLimits_bypassFrequentistFit" ]]; then
     time parallel --progress --jobs 10 'mass={1}; year={2}; coupling={3}; signal={4}; combine_method={5};
 echo \"====================================================================\"; echo $mass; datacardfile=${mainpath}/${method}/${year}/diphoton_combine_${mass}_DiPhotons_${coupling}_${year}.txt;
 echo $datacardfile; echo \"combine -M AsymptoticLimits -s -1 --bypassFrequentistFit $datacardfile\";
@@ -43,7 +43,7 @@ combine -M AsymptoticLimits  -s -1 $datacardfile > ${datacardfile}_results;
 mv higgsCombineTest.AsymptoticLimits.mH*.root combineJobs13TeV/${year}/${signal}/${coupling}/${combine_method}/All/.;' ::: $(seq ${massMin} ${massInterval} ${massMax}) ::: ${year} ::: ${coupling} ::: ${signal} ::: $combine_method
 fi
 
-for mass in masslist; do
+for mass in $masslist; do
     datacardfile=${mainpath}/${method}/${year}/diphoton_combine_${mass}_DiPhotons_${coupling}_${year}.txt
     obs=$(cat ${datacardfile}_results  | grep  "Observed Limit:" | awk '{print $5}')
     expM2s=$(cat ${datacardfile}_results  | grep  "Expected  2.5%:" | awk '{print $5}')
