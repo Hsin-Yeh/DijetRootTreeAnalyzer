dde6985da98ff33a1af46887108b84cffbd97902
HsinYeh <thankyouyou06@gmail.com>
2023-05-05 15:26:45 +0800

    update

--- runHeavyHiggs.sh
+++ runHeavyHiggs.sh
@@ -6,7 +6,7 @@ export method="genFiducial"
 export InterpolateShapePath="/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/DijetShapeInterpolator/${method}"
 export configFile="config/diphotons_500GeV.config"
 export bkgFitResultsPath="test_directory"
-export SignalNormFile="/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/SignalNorm_Splines_${method}.txt"
+export SignalNormFile="/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/SignalNorm_genFiducial.txt"
 export massInterval=100
 
 ############################## signal Interpolation ##############################
