96e2f89679b86745b5b4355a50658dd46a00cba5
HsinYeh <thankyouyou06@gmail.com>
2023-05-07 17:17:04 +0800

    update

--- python/plotNLL.py
+++ python/plotNLL.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
         nll0 = root_numpy.tree2array(tree,'nll0')
         deltaNLL = root_numpy.tree2array(tree,'deltaNLL')
         total = 2*(deltaNLL+nll+nll0)
-        import matplotlib.pyplot as plt
-        plt.scatter(r,total)
-        plt.savefig("test.png")
+        print(r, nll, nll0, deltaNLL, total)
+        # import matplotlib.pyplot as plt
+        # plt.scatter(r,total)
+        # plt.savefig("test.png")
