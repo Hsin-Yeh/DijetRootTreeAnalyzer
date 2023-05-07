d22f2b0c469205c93e940af3c5f83f955741d027
HsinYeh <thankyouyou06@gmail.com>
2023-05-07 18:01:49 +0800

    Update

--- python/plotNLL.py
+++ python/plotNLL.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
         ax.scatter(r,total,color=color_template[ifile],label=name)
 
     ax.legend()
-    ax.xlim([-1,0])
+    ax.set_xlim([-1,0])
     plt.savefig("test.png")
