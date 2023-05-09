68b27dd54752b9cee9e5dc3714e549775ff29571
HsinYeh <thankyouyou06@gmail.com>
2023-05-09 21:16:12 +0800

    update

--- python/CompareShapes_interpolate_fit.py
+++ python/CompareShapes_interpolate_fit.py
@@ -136,10 +136,11 @@ if __name__ == '__main__':
         MH  = wsparamshape.var("MH")
         MH.setVal(options.mass);
         varbin = ROOT.RooBinning(len(binBoundaries[options.coup])-1, array('d', binBoundaries[options.coup]))
+        # tbins = ROOT.RooBinning(12);
         # p = mgg.frame()
         p = mgg.frame(0,1000,125)
         shape = wsparamshape.pdf("SignalShape_%s_EBEB"%(options.coup));
-        shape.plotOn(p,ROOT.RooFit.DrawOption("B"),ROOT.RooFit.LineColor(4))
+        shape.plotOn(p,ROOT.RooFit.DrawOption("B"),ROOT.RooFit.Binning(varbin),ROOT.RooFit.LineColor(4))
         # hist_mass_list_rsg = ROOT.TH1D()
 
         # #Now to the interpolated shape
