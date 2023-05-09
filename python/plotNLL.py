94de44b1ae40b5af5ac82bb0bdd2be1f98e43a6d
HsinYeh <thankyouyou06@gmail.com>
2023-05-10 02:41:40 +0800

    update

--- python/plotNLL.py
+++ python/plotNLL.py
@@ -23,7 +23,7 @@ parser.add_argument('--mass',default=750,type=int,help='plot name mass')
 
 args = parser.parse_args()
 
-def average_total(array):
+def average_array(array):
     count=0
     average_total=0
     for num in array:
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         deltaNLL = root_numpy.tree2array(tree,'deltaNLL')
         total = 2*(deltaNLL+nll+nll0)
         print(r, nll, nll0, deltaNLL, total)
-        average_total = average_total(total)
+        average_total = average_array(total)
         ax.scatter(r,total,color=color_template[ifile],linestyle=linestyle,label=name)
 
     ax.legend()
