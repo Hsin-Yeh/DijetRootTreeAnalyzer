e62e65a6f98192d0df8a8a73a4dacb5a96976a5b
HsinYeh <thankyouyou06@gmail.com>
2023-05-09 17:31:11 +0800

    update

--- python/CompareShapes_interpolate_fit.py
+++ python/CompareShapes_interpolate_fit.py
@@ -201,10 +201,10 @@ if __name__ == '__main__':
         # pave_gof = ROOT.TPaveText(0.65,0.85,0.95,1.0,"NDC")
 
         p.SetLineColor(1)
-        p.GetXaxis().SetRangeUser(massMin, massMax)
+        # p.GetXaxis().SetRangeUser(massMin, massMax)
         p.GetXaxis().SetTitle("DiPhoton mass [GeV]")
         p.GetYaxis().SetTitle("Normalized yield/bin width")
-        p.Scale(1/h_mgg_fit.GetSumOfWeights())
+        # p.Scale(1/h_mgg_fit.GetSumOfWeights())
         p.Draw("hist")
         # hist_mass_list_rsg.GetXaxis().SetRangeUser(massMin, massMax)
         # hist_mass_list_rsg.SetLineColor(2)
