851c88c7b18f8cbdf95d826e2f586b30c6d0717b
HsinYeh <thankyouyou06@gmail.com>
2023-05-10 12:44:34 +0800

    update

--- python/CompareShapes.py
+++ python/CompareShapes.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
             histos.append(h_mgg_varbins)
 
             #Now to the interpolated shape
-            tfileRes = rt.TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/DijetShapeInterpolator/%s/ResonanceShapes_InputShapes_%s_%s_%s.root" %(method,title,options.cat,year) , "read")
+            tfileRes = rt.TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/DijetShapeInterpolator/%s/ResonanceShapes_InputShapes_%s_%s_%s.root" %(options.method,title,options.cat,year) , "read")
             tfileRes.cd()
 
             #tfileRes.Print()
