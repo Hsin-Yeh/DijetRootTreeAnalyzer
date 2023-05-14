d7dffe41b01ffc1d5dd9511a2437c610e1d4e65f
HsinYeh <thankyouyou06@gmail.com>
2023-05-14 21:58:19 +0800

    update

--- python/BinnedFit.py
+++ python/BinnedFit.py
@@ -1427,7 +1427,7 @@ if __name__ == '__main__':
     rootFile.Close()
     
 
-    outFileName = "FitResults_%s_%s.root"%(box,options.year)
+    outFileName = "FitResults_%s_%s.root"%(box)
     outFile = rt.TFile.Open(options.outDir+"/"+outFileName,'recreate')
     outFile.cd()
     w.Write()
