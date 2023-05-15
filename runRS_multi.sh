6937dcf88153ddf9157cae056197c71456787458
HsinYeh <thankyouyou06@gmail.com>
2023-05-16 02:57:07 +0800

    update

--- runRS_multi.sh
+++ runRS_multi.sh
@@ -12,9 +12,9 @@ export SignalNormFile="/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSS
 
 # Run flags
 export binnedFit_flag=false
-export writeDataCard_flag=true
+export writeDataCard_flag=false
 export combineCard_flag=true
-export combineLimit_flag=true
+export combineLimit_flag=false
 
 # ############################## signal interpolation ##############################
 
