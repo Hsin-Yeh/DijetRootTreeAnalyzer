229ca1bf4ae1c2329d7b2daac6f13aa2e1db4fa6
HsinYeh <thankyouyou06@gmail.com>
2023-05-18 01:56:32 +0800

    update

--- python/plotNLL.py
+++ python/plotNLL.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
         elif (in_filename.find("pdf_1") != -1): name="expow1"
         elif (in_filename.find("pdf_2") != -1): name="invpowlin1"
         if (in_filename.find("Envelope") != -1):
-            ax.scatter(r,total,facecolors='none', edgecolors='r',s=30)
+            ax.scatter(r,total,facecolors='none', edgecolors='r',s=40)
         else:
             ax.scatter(r,total,color=color_template[ifile],label=name)
 
