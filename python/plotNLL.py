f16bd662247a2518520308eb33fd3cf1b3b8f9ba
HsinYeh <thankyouyou06@gmail.com>
2023-05-18 01:53:37 +0800

    update

--- python/plotNLL.py
+++ python/plotNLL.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         if (in_filename.find("Envelope") != -1):
             ax.scatter(r,total,facecolors='none', edgecolors='r',markersize=20)
         else:
-            ax.scatter(r,total,color=color_template[ifile],linestyle=linestyle,label=name)
+            ax.scatter(r,total,color=color_template[ifile],label=name)
 
 
     ax.legend(loc="upper left")
