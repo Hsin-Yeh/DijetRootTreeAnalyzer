0e6e376396d440229c0de711fe96049664b37585
HsinYeh <thankyouyou06@gmail.com>
2023-05-18 03:33:53 +0800

    update

---
+++ python/compareSystematics.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+import ROOT
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('in_filenames',nargs="+",help='input filenames')
+parser.add_argument('--outputDir','-o',default="./output",type=str,help='output directory')
+parser.add_argument('--report','-r',default=10000,type=int,help='report every x events')
+parser.add_argument('--debug','-d',action="store_true",help='debug mode')
+args = parser.parse_args()
+
+coup="kMpl01"
+mass="1000"
+year="2017"
+c1 = ROOT.TCanvas()
+for ifile, in_filename in enumerate(args.in_filenames):
+    infile = ROOT.TFile.Open(in_filename)
+    infile.cd()
+    h = infile.Get("h_RSGravitonToGammaGamma_%s_M%s_%s"%(coup, mass, year))
+    if( ifile == 0 ):
+        h.Draw()
+    else:
+        h.Draw("same")
+
+c1.SaveAs("test.png")
