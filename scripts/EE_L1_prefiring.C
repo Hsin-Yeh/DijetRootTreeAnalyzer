0237d2d7212f9a8181b1da1cf2de9b75046bc796
HsinYeh <thankyouyou06@gmail.com>
2023-05-26 11:46:32 +0800

    update

---
+++ scripts/EE_L1_prefiring.C
@@ -0,0 +1,30 @@
+void EE_L1_prefiring(){
+
+    std::string reweightString1="";
+    std::string reweightString2="";
+
+    TFile *corrFile2016 = new TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/CMSDIJET/DijetRootTreeAnalyzer/data/EE_L1_prefiring/L1prefiring_photonpt_2016BtoH.root");
+    TH2D *corrHist2016 = (TH2D*)corrFile2016->Get("L1prefiring_photonpt_2016BtoH");
+    TFile *corrFile2017 = new TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/CMSDIJET/DijetRootTreeAnalyzer/data/EE_L1_prefiring/L1prefiring_photonpt_2017BtoF.root");
+    TH2D *corrHist2017 = (TH2D*)corrFile2017->Get("L1prefiring_photonpt_2017BtoF");
+
+    int Nbins = corrHist2016->GetNbinsX()*corrHist2016->GetNbinsY();
+    for(int ibin=0; ibin<Nbins+1; ibin++){
+        double weight = 1 - corrHist2016->GetBinContent(ibin); // The content is prefire rate. The weight is non-prefiring probability
+        double weightError = -corrHist2016->GetBinError(ibin); // minus sign to give the correct non-prefiring probability uncertainty
+        int xbin,ybin,zbin;
+        corrHist2016->GetBinXYZ(ibin,xbin,ybin,zbin);
+        int etaLow = corrHist2016->GetXaxis()->GetBinLowEdge(xbin);
+        int etaUp = corrHist2016->GetXaxis()->GetBinUpEdge(xbin);
+        int ptLow = corrHist2016->GetYaxis()->GetBinLowEdge(ybin);
+        int ptUp = corrHist2016->GetYaxis()->GetBinUpEdge(ybin);
+        std::cout << ibin << " " << xbin << " " << ybin << std::endl;
+        std::cout << Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, sigma) << std::endl;
+            // reweightString1 += "(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)" % (ptLow, ptUp, etaLow, etaUp, weight, weightError, sigma)
+            // if (ibin != Nbins-1): reweightString1 += "+"
+        // reweightString2 = reweightString1.replace("ph1","ph2")
+        // reweight = "(" + reweightString1 + ")*(" + reweightString2 + ")"
+        // print count
+        // print Nbins
+    }
+}
