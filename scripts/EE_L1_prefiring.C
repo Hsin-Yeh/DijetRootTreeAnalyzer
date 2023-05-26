b6bb0a11a1c2e8ce619a86631f429eea24fa734a
HsinYeh <thankyouyou06@gmail.com>
2023-05-26 12:10:21 +0800

    update

--- scripts/EE_L1_prefiring.C
+++ scripts/EE_L1_prefiring.C
@@ -23,8 +23,8 @@ void EE_L1_prefiring(){
         double etaUp = corrHist2016->GetXaxis()->GetBinUpEdge(xbin);
         double ptLow = corrHist2016->GetYaxis()->GetBinLowEdge(ybin);
         double ptUp = corrHist2016->GetYaxis()->GetBinUpEdge(ybin);
-        std::cout << ibin << " " << xbin << " " << ybin << std::endl;
-        std::cout << Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0) << std::endl;
+        // std::cout << ibin << " " << xbin << " " << ybin << std::endl;
+        // std::cout << Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0) << std::endl;
         reweightString1 += Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0);
         reweightString2 += Form("(ph2pt>=%f && ph2pt<%f && ph2scEta>=%f && ph2scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0);
         if (ibin != Nbins-1){
@@ -37,4 +37,36 @@ void EE_L1_prefiring(){
     reweight = "(" + reweightString1 + ")*(" + reweightString2 + ")";
     std::cout << reweight << std::endl;
     std::cout <<count <<std::endl;
+
+
+    reweight="";
+    reweightString1="";
+    reweightString2="";
+    count = 0;
+    for(int ibin=0; ibin<Nbins; ibin++){
+        double weight = 1 - corrHist2017->GetBinContent(ibin); // The content is prefire rate. The weight is non-prefiring probability
+        double weightError = -corrHist2017->GetBinError(ibin); // minus sign to give the correct non-prefiring probability uncertainty
+        if (weight == 1) continue;
+        count++;
+        int xbin,ybin,zbin;
+        corrHist2017->GetBinXYZ(ibin,xbin,ybin,zbin);
+        double etaLow = corrHist2017->GetXaxis()->GetBinLowEdge(xbin);
+        double etaUp = corrHist2017->GetXaxis()->GetBinUpEdge(xbin);
+        double ptLow = corrHist2017->GetYaxis()->GetBinLowEdge(ybin);
+        double ptUp = corrHist2017->GetYaxis()->GetBinUpEdge(ybin);
+        // std::cout << ibin << " " << xbin << " " << ybin << std::endl;
+        // std::cout << Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0) << std::endl;
+        reweightString1 += Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0);
+        reweightString2 += Form("(ph2pt>=%f && ph2pt<%f && ph2scEta>=%f && ph2scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0);
+        if (ibin != Nbins-1){
+            reweightString1 += "+";
+            reweightString2 += "+";
+        }
+    }
+    reweightString1.pop_back();
+    reweightString2.pop_back();
+    reweight = "(" + reweightString1 + ")*(" + reweightString2 + ")";
+    std::cout << reweight << std::endl;
+    std::cout <<count <<std::endl;
+
 }
