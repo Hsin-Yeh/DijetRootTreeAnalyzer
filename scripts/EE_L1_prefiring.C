99299c899bb3f9dc9220430a2261f2b6a1a846ad
HsinYeh <thankyouyou06@gmail.com>
2023-05-26 12:05:08 +0800

    update

--- scripts/EE_L1_prefiring.C
+++ scripts/EE_L1_prefiring.C
@@ -1,5 +1,6 @@
 void EE_L1_prefiring(){
 
+    std::string reweight;
     std::string reweightString1="";
     std::string reweightString2="";
 
@@ -9,8 +10,9 @@ void EE_L1_prefiring(){
     TH2D *corrHist2017 = (TH2D*)corrFile2017->Get("L1prefiring_photonpt_2017BtoF");
 
     int count = 0;
-    int Nbins = (corrHist2016->GetNbinsX()+2)*(corrHist2016->GetNbinsY()+2);
-    for(int ibin=0; ibin<Nbins+1; ibin++){
+    // int Nbins = (corrHist2016->GetNbinsX()+2)*(corrHist2016->GetNbinsY()+2);
+    int Nbins = 350;
+    for(int ibin=0; ibin<Nbins; ibin++){
         double weight = 1 - corrHist2016->GetBinContent(ibin); // The content is prefire rate. The weight is non-prefiring probability
         double weightError = -corrHist2016->GetBinError(ibin); // minus sign to give the correct non-prefiring probability uncertainty
         if (weight == 1) continue;
@@ -23,12 +25,16 @@ void EE_L1_prefiring(){
         double ptUp = corrHist2016->GetYaxis()->GetBinUpEdge(ybin);
         std::cout << ibin << " " << xbin << " " << ybin << std::endl;
         std::cout << Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0) << std::endl;
-        // reweightString1 += Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)" % (ptLow, ptUp, etaLow, etaUp, weight, weightError, sigma))
-        //     if (ibin != Nbins-1): reweightString1 += "+"
-        // reweightString2 = reweightString1.replace("ph1","ph2")
-        // reweight = "(" + reweightString1 + ")*(" + reweightString2 + ")"
-        // print count
-        // print Nbins
+        reweightString1 += Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)" % (ptLow, ptUp, etaLow, etaUp, weight, weightError, 0));
+        reweightString2 += Form("(ph2pt>=%f && ph2pt<%f && ph2scEta>=%f && ph2scEta<%f)*(%f + %f*%d)" % (ptLow, ptUp, etaLow, etaUp, weight, weightError, 0));
+        if (ibin != Nbins-1){
+            reweightString1 += "+";
+            reweightString2 += "+";
+        }
     }
+    reweightString1 -= "+";
+    reweightString2 -= "+";
+    reweight = "(" + reweightString1 + ")*(" + reweightString2 + ")";
+    std::cout << reweight << std::endl;
     std::cout <<count <<std::endl;
 }
