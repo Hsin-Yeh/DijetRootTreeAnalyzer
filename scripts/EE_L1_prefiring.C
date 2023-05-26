c0450bcab9db89339e00ed70d8f3e9023fa48232
HsinYeh <thankyouyou06@gmail.com>
2023-05-26 12:06:55 +0800

    update

--- scripts/EE_L1_prefiring.C
+++ scripts/EE_L1_prefiring.C
@@ -26,7 +26,7 @@ void EE_L1_prefiring(){
         std::cout << ibin << " " << xbin << " " << ybin << std::endl;
         std::cout << Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0) << std::endl;
         reweightString1 += Form("(ph1pt>=%f && ph1pt<%f && ph1scEta>=%f && ph1scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0);
-        reweightString2 += Form("(ph2pt>=%f && ph2pt<%f && ph2scEta>=%f && ph2scEta<%f)*(%f + %f*%d)" ptLow, ptUp, etaLow, etaUp, weight, weightError, 0);
+        reweightString2 += Form("(ph2pt>=%f && ph2pt<%f && ph2scEta>=%f && ph2scEta<%f)*(%f + %f*%d)",ptLow, ptUp, etaLow, etaUp, weight, weightError, 0);
         if (ibin != Nbins-1){
             reweightString1 += "+";
             reweightString2 += "+";
