07a21c80fddb75b91678a57211ed991efad474f2
HsinYeh <thankyouyou06@gmail.com>
2023-05-26 12:08:05 +0800

    update

--- scripts/EE_L1_prefiring.C
+++ scripts/EE_L1_prefiring.C
@@ -32,6 +32,8 @@ void EE_L1_prefiring(){
             reweightString2 += "+";
         }
     }
+    reweightString1.pop_back();
+    reweightString2.pop_back();
     reweight = "(" + reweightString1 + ")*(" + reweightString2 + ")";
     std::cout << reweight << std::endl;
     std::cout <<count <<std::endl;
