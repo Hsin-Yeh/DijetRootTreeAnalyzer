bb93634702595d79b9453c656afd53b97bc8c56d
HsinYeh <thankyouyou06@gmail.com>
2023-05-28 15:07:36 +0800

    update

--- python/plotNorm.py
+++ python/plotNorm.py
@@ -17,14 +17,17 @@ args = parser.parse_args()
 if __name__ == "__main__":
 
     c1 = ROOT.TCanvas()
+    pad1 = ROOT.TPad("pad1","",0.05,0.40,0.95,0.95)
+    pad2 = ROOT.TPad("pad2","",0.05,0.05,0.95,0.35)
+    pad1.cd()
     l = ROOT.TLegend(0.6, 0.55, 0.8, 0.7)
     luminosity = {"2016":35.9, "2017":41.5, "2018":59.7}
-    color={"All":1,"EBEB":2,"EBEE":4}
+    color=[[1,2,4], [5,6,8]]
     linestyle=[1,1,10]
     markerstyle=[24,26,28]
 
     g_EBEB, g_EBEE, g_All = {},{},{}
-
+    EBEBnorms, EBEEnorms, Allnorms, masses = {}, {}, {}, {}
     for ifile, in_filename in enumerate(args.in_filenames):
         EBEBnorm, EBEEnorm, Allnorm, mass =  array('d'), array('d') , array('d'), array('d')
         with open (in_filename,'r') as infile:
@@ -36,6 +39,9 @@ if __name__ == "__main__":
                 elif (line.find("All")!=-1):
                     Allnorm.append(float(line.split(" ")[4])/luminosity[args.year])
                     mass.append(float(line.split(" ")[2]))
+        EBEBnorms.append(EBEBnorm)
+        EBEEnorms.append(EBEEnorm)
+        Allnorms.append(Allnorm)
         g_EBEB[ifile] = ROOT.TGraph(len(mass), mass, EBEBnorm)
         g_EBEE[ifile] = ROOT.TGraph(len(mass), mass, EBEEnorm)
         g_All[ifile] = ROOT.TGraph(len(mass), mass, Allnorm)
@@ -45,23 +51,23 @@ if __name__ == "__main__":
         g_All[ifile].GetYaxis().SetTitle("Normalization")
         g_All[ifile].SetMarkerColor(color["All"])
         g_All[ifile].SetMarkerSize(1)
-        g_All[ifile].SetLineColor(color["All"])
+        g_All[ifile].SetLineColor(color[0][ifile])
         g_All[ifile].SetLineWidth(1)
         g_All[ifile].SetLineStyle(linestyle[ifile])
         g_All[ifile].SetMarkerStyle(markerstyle[ifile])
         if (ifile==0): g_All[ifile].Draw("AL")
         else: g_All[ifile].Draw("LSame")
         g_All[ifile].GetYaxis().SetRangeUser(0,1)
-        g_EBEB[ifile].SetMarkerColor(color["EBEB"])
+        g_EBEB[ifile].SetMarkerColor(color[1][ifile])
         g_EBEB[ifile].SetMarkerSize(1)
-        g_EBEB[ifile].SetLineColor(color["EBEB"])
+        g_EBEB[ifile].SetLineColor(color[1][ifile])
         g_EBEB[ifile].SetLineWidth(1)
         g_EBEB[ifile].SetLineStyle(linestyle[ifile])
         g_All[ifile].SetMarkerStyle(markerstyle[ifile])
         g_EBEB[ifile].Draw("LSame")
-        g_EBEE[ifile].SetMarkerColor(color["EBEE"])
+        g_EBEE[ifile].SetMarkerColor(color[2][ifile])
         g_EBEE[ifile].SetMarkerSize(1)
-        g_EBEE[ifile].SetLineColor(color["EBEE"])
+        g_EBEE[ifile].SetLineColor(color[2][ifile])
         g_EBEE[ifile].SetLineWidth(1)
         g_EBEE[ifile].SetLineStyle(linestyle[ifile])
         g_EBEE[ifile].Draw("LSame");
@@ -70,5 +76,17 @@ if __name__ == "__main__":
         # l.AddEntry(g_FC, "FC", "P")
         # l.AddEntry(g_ZFC, "ZFC", "P")
         # l.Draw("same")
+
+    pad2.cd()
+    EBEBnorm_diff, EBEEnorm_diff, All_diff= array('d'), array('d'), array('d')
+    EBEBnorm_diff=EBEBnorm[0]/EBEBnorm[1]
+    EBEEnorm_diff=EBEEnorm[0]/EBEEnorm[1]
+    Allnorm_diff=Allnorm[0]/Allnorm[1]
+    g_diff_EBEB = ROOT.TGraph(len(masses[0]), masses[0], EBEBnorm_diff)
+    g_diff_EBEE = ROOT.TGraph(len(masses[0]), masses[0], EBEEnorm_diff)
+    g_diff_All = ROOT.TGraph(len(masses[0]), masses[0], Allnorm_diff)
+    g_diff_EBEB.Draw("AL")
+    g_diff_EBEE.Draw("LSame")
+    g_diff_All.Draw("LSame")
 
     c1.SaveAs("test.png")
