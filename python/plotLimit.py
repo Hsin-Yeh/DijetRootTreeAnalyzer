2cd276f38f1cdccde0cfe0490606f278068177f3
HsinYeh <thankyouyou06@gmail.com>
2023-05-28 16:18:58 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     mass_array_reversed = array('d',reversed(mass_array))
     expP1s_array_reversed = array('d',reversed(expP1s_array))
     expP2s_array_reversed = array('d',reversed(expP2s_array))
+    mass_long_array = array('d',mass_array + mass_array_reversed)
     exp1s_array = array('d',expM1s_array + expP1s_array_reversed)
     exp2s_array = array('d',expM2s_array + expP2s_array_reversed)
 
