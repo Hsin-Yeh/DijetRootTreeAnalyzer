152076975d09ed868dc04a4f8a91e37c5a5539da
HsinYeh <thankyouyou06@gmail.com>
2023-06-03 16:34:02 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -37,7 +37,6 @@ def Acceptance(year, coupling, mass):
         for line in Lines:
             y, c, m, cat, norm = line.split()
             if (c==coupling and m==mass and cat=='All'):
-                print (year, coupling, mass, cat, norm)
                 totalNorm += float(norm)
         totalNorm = float(totalNorm)/float(lumi(year))
     else:
