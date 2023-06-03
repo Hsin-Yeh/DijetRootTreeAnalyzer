23364eed9a2d3947edcf10f8279c735151feb468
HsinYeh <thankyouyou06@gmail.com>
2023-06-03 17:05:57 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -85,6 +85,7 @@ if __name__ == "__main__":
         Lines = infile.readlines()
     for line in Lines:
         mass, obs, expP2s, expP1s, exp, expM1s, expM2s = line.split()
+        print('--------------------%s--------------------'%mass)
         norm = Acceptance(args.year, args.coupling, mass)
         mass_array.append(float(mass))
         obs_array.append(float(obs)/norm)
