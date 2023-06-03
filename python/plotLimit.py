cb07c55785635a20b86a038a92a6023aa10bc3b1
HsinYeh <thankyouyou06@gmail.com>
2023-06-03 16:58:25 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -52,7 +52,7 @@ def Acceptance(year, coupling, mass):
 if __name__ == "__main__":
 
     df = pd.read_csv('/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/SignalNorm_Splines_full.txt', sep=" ")
-    print(df.loc[df['year'] == args.year, 'A'].iloc[0])
+    df.query('year==%s',args.year)['norm']
     # i=df.loc[df['year'] == args.year]
 
 
