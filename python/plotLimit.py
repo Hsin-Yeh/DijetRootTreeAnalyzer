5a7fb95bdf09435aa9aeb1335a0fc73b0fc19f29
HsinYeh <thankyouyou06@gmail.com>
2023-06-11 13:12:28 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     exp2SGraph.GetYaxis().SetTitle("95% CL limit #sigma(pp#rightarrowG#rightarrow#gamma#gamma) (fb)" if args.signame == "grav" else "95% CL limit #sigma(pp#rightarrowS#rightarrow#gamma#gamma) (fb)" );
     exp2SGraph.GetXaxis().SetTitle("m_{G} (GeV)" if args.signame == "grav" else "m_{S} (GeV)");
     exp2SGraph.GetXaxis().SetMoreLogLabels();
-    exp2SGraph.GetXaxis().SetRangeUser(600,7000);
+    exp2SGraph.GetXaxis().SetRangeUser(600,5000);
     exp2SGraph.SetTitle("")
 
     exp2SGraph.Draw("AF");
