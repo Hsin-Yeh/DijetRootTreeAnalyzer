a562570c9f327eed7fed38a8a7c3818fc93c1f5d
HsinYeh <thankyouyou06@gmail.com>
2023-06-24 01:10:23 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     exp2SGraph.GetXaxis().SetTitle("m_{G} (GeV)" if args.signame == "grav" else "m_{S} (GeV)");
     exp2SGraph.GetXaxis().SetMoreLogLabels();
     if (args.coupling=="kMpl01" or args.coupling=="kMpl02"): exp2SGraph.GetXaxis().SetRangeUser(600,7000)
-    else: exp2SGraph.GetXaxis().SetRangeUser(600,5000)
+    else: exp2SGraph.GetXaxis().SetRangeUser(800,5000)
     exp2SGraph.SetTitle("")
 
     exp2SGraph.Draw("AF");
