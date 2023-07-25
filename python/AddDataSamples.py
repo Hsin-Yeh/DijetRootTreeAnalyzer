d0b31e51b3064aa4ce0225ad696e88b364479f39
HsinYeh <thankyouyou06@gmail.com>
2023-07-26 00:43:31 +0800

    update

--- python/AddDataSamples.py
+++ python/AddDataSamples.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
         histos.append(h_mgg_100GeVbin)
 
         for i in range(1,80):
-            print("%d, %d-%dGeV, %d",i, 500+100*(i-1), 500+100*i, h_mgg_100GeVbin.GetContent(i))
+            print("%d, %d-%dGeV, %d",i, 500+100*(i-1), 500+100*i, h_mgg_100GeVbin.GetBinContent(i))
 
 
     if options.type=='nom':
