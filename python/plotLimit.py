14c12119ca9d1aa74d685ae99d67f47e8404b16b
HsinYeh <thankyouyou06@gmail.com>
2023-08-03 23:39:25 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -50,7 +50,6 @@ def Acceptance(year, coupling, mass):
             break
     totalNorm = float(totalNorm)/float(lumi(year))
     if (year!="fullRun2"): totalNorm = 0.85
-    totalNorm = 1
     return totalNorm
 
 if __name__ == "__main__":
