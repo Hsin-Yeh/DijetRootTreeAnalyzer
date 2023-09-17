1e2038f9d1bbc7ad07de73b31d11d9ec318249f0
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-09-17 16:45:51 +0800

    update

--- run_multi_combine.sh
+++ run_multi_combine.sh
@@ -72,10 +72,10 @@ export SignalNormFile="/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSS
 export datacard_configfile="config/diphotons_bias_${year}_pdf_index_wopip_wopil.config"
 
 #################### Run flags ####################
-export binnedFit_flag=false
+export binnedFit_flag=true
 export writeDataCard_flag=false
 export combineCard_flag=false
-export combineLimit_flag=true
+export combineLimit_flag=false
 
 # ############################## bkg model ##############################
 if $binnedFit_flag; then
