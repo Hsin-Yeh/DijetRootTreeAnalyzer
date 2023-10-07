3e316cadf6d5ca025725be88e4124a8e7d3d308d
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-10-07 22:32:11 +0800

    AddSignalShapes width version

---
+++ python/AddSignalShapes_width.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import ROOT as rt
+import rootTools
+from framework import Config
+from array import *
+import os
+import sys
+
+def project(tree, h, var, cut):
+    print 'projecting var: %s, cut: %s from tree: %s into hist: %s'%(var, cut, tree.GetName(), h.GetName())
+    tree.Project(h.GetName(),var,cut)
+
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option('-t','--type',dest="type",default="nom",type="string",
+                  help="type of result")
+    parser.add_option('-m','--model',dest="model",default="gg",type="string",
+                  help="model")
+    parser.add_option('-c','--cat',dest="cat",default="EBEB",type="string",
+                  help="category")
+    parser.add_option('-d','--outdir',dest="outDir",default="./",type="string",
+                  help="Output directory to store output histograms")
+    parser.add_option('-s','--sys',dest="sys",default="energyScaleStat",type="string",
+                  help="Systematics")
+    parser.add_option("-e", "--method",dest="method",default="full",type="string",
+                  help="Choose full range or truncate with the fwhm mass range")
+
+    (options,args) = parser.parse_args()
+    if options.type=='nom':
+        TYPE = ''
+    else:
+        TYPE = options.type.upper()
+
+    histos = []
+    year = ''
+
+    energySyslist = {"energyScaleStatUp"   : "ph1energyScaleStatUp*ph2energyScaleStatUp",
+                     "energyScaleSystUp"   : "ph1energyScaleSystUp*ph2energyScaleSystUp",
+                     "energyScaleGainUp"   : "ph1energyScaleGainUp*ph2energyScaleGainUp",
+                     "energySigmaUp"       : "ph1energySigmaUp*ph2energySigmaUp",
+
+                     "energyScaleStatDown"   : "ph1energyScaleStatDown*ph2energyScaleStatDown",
+                     "energyScaleSystDown"   : "ph1energyScaleSystDown*ph2energyScaleSystDown",
+                     "energyScaleGainDown"   : "ph1energyScaleGainDown*ph2energyScaleGainDown",
+                     "energySigmaDown"       : "ph1energySigmaDown*ph2energySigmaDown" }
+
+    SFSyslist     = {"nom" : "ph1SF*ph2SF",
+                     "SFUp" : "ph1SFUp*ph2SFUp",
+                     "SFDown" : "ph1SFDown*ph2SFDown"}
+
+    EEPFSyslist   = {"nom" : "ph1EEPF*ph2EEPF",
+                     "EEPFUp" : "ph1EEPFUp*ph2EEPFUp",
+                     "EEPFDown" : "ph1EEPFDown*ph2EEPFDown"}
+
+    PuSyslist   = {"nom" : "weightPuManual",
+                   "PuUp" : "weightPuManualUp",
+                   "PuDown" : "weightPuManualDown"}
+
+    acc={}
+    eff={}
+    acc["EBEB"] = "(mgg > 500 && deltaR > 0.45 && ph1pt>125 && ph2pt>125 && isEBEB)"
+    acc["EBEE"] = "(mgg > 500 && deltaR > 0.45 && ph1pt>125 && ph2pt>125 && (isEBEE || isEEEB))"
+    eff["2016"] = "isGood*(HLT_DoublePhoton60 || HLT_ECALHT800)"
+    eff["2017"] = "isGood*(HLT_DoublePhoton70 || HLT_ECALHT800)"
+    eff["2018"] = "isGood*(HLT_DoublePhoton70 || HLT_ECALHT800)"
+
+    for f in args:
+        year = f.split('.root')[0].split('_')[-1]
+        mass = int(f.split('_M_')[1].split('_TuneCP2')[0])
+        coup = f.split('_M_')[0].split('/')[-1].split('_')[-1]
+        title = f.split('/')[-1].split("_")[0] + "_M_" + str(mass)
+
+        print(title,year,mass,coup)
+        tfileIn = rt.TFile.Open(f)
+        thetree=tfileIn.Get("HighMassDiphoton")
+
+        # h_mgg_1GeVbin = rt.TH1D('h_mgg_1GeVbin','h_mgg_1GeVbin',14000,0,14000)
+        #h_mgg_5GeVbin = rt.TH1D('h_mgg_5GeVbin','h_mgg_5GeVbin',2800,0,14000)
+        h_mgg_8GeVbin = rt.TH1D('h_mgg_8GeVbin','h_mgg_8GeVbin',1750, 0, 14000)
+        h_mgg_ratio = rt.TH1D('h_mgg_ratio','h_mgg_ratio',1000,0,2.0)
+
+        if options.sys.find("Up")!=-1: numSigma = 1
+        elif options.sys.find("Down")!=-1: numSigma = -1
+        else: numSigma = 0
+
+        nomCut = '(' + acc[options.cat] + ')*(' + eff[year] + ')*(' + SFSyslist["nom"] + ')*(' + EEPFSyslist["nom"] + ')*(' + PuSyslist["nom"] + ')*(weightAll)'
+        genMassCut = 'mggGen>%f && mggGen<%f'%(mass*0.8, mass*1.2)
+
+
+        if options.method=="full":          allCuts = nomCut
+        elif options.method=="truncate":    allCuts = '(' + nomCut + ')*(' + fwhmCut + ')'
+        elif options.method=="genFiducial": allCuts = '(' + nomCut + ')*(' + genMassCut + ')'
+
+        if options.type=='nom':
+            project(thetree,h_mgg_ratio, "mgg/%f"%(float(mass)), allCuts )
+            project(thetree,h_mgg_8GeVbin, "mgg", allCuts )
+        elif options.sys in energySyslist:
+            project(thetree,h_mgg_ratio, "mgg*%s/%f"%(energySyslist[options.sys],float(mass)), allCuts )
+
+        elif options.sys in SFSyslist:
+            allCuts = '(' + acc[options.cat] + ')*(' + eff[year] + ')*(' + SFSyslist[options.sys] + ')*(' + EEPFSyslist["nom"] + ")*(" + PuSyslist["nom"] + ")*(weightAll)"
+            project(thetree,h_mgg_ratio, "mgg/%f"%(float(mass)), allCuts )
+
+        elif options.sys in PuSyslist:
+            allCuts = '(' + acc[options.cat] + ')*(' + eff[year] + ')*(' + SFSyslist["nom"] + ')*(' + EEPFSyslist["nom"] + ")*(" + PuSyslist[options.sys] + ")*(weightAll)"
+            project(thetree,h_mgg_ratio, "mgg/%f"%(float(mass)), allCuts )
+
+        elif options.sys in EEPFSyslist:
+            allCuts = '(' + acc[options.cat] + ')*(' + eff[year] + ')*(' + SFSyslist["nom"] + ')*(' + EEPFSyslist[options.sys] + ")*(" + PuSyslist["nom"] + ")*(weightAll)"
+            project(thetree,h_mgg_ratio, "mgg/%f"%(float(mass)), allCuts )
+
+        #h = tfileIn.Get('h_mjj_ratio_%s'%options.type)
+        h_mgg_ratio.SetName('h_%s_%s_%s'%(title,coup,year))
+        h_mgg_ratio.SetTitle('h_%s_%s_%s'%(title,coup,year))
+        h_mgg_ratio.SetDirectory(0)
+        histos.append(h_mgg_ratio)
+        h_mgg_8GeVbin.SetName('h_%s_%s_%s_8Gev'%(title,coup,year))
+        h_mgg_8GeVbin.SetTitle('h_%s_%s_%s_8GeV'%(title,coup,year))
+        h_mgg_8GeVbin.SetDirectory(0)
+        histos.append(h_mgg_8GeVbin)
+
+
+
+    if options.type=='nom':
+        tfileOut = rt.TFile.Open('%s/InputShapes_%s_%s_%s.root'%(options.outDir,title,options.cat,year),'recreate')
+    else:
+        tfileOut = rt.TFile.Open('%s/InputShapes_%s_%s_%s_%s.root'%(options.outDir,title,options.cat,year,options.sys),'recreate')
+    tfileOut.cd()
+    for h in histos:
+        h.Write()
+    tfileOut.Close()
---
+++ python/CompareShapes_extrapolate.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+from optparse import OptionParser
+import ROOT
+
+from ROOT import TCanvas, TColor, TGaxis, TH1F, TPad
+from ROOT import kBlack, kBlue, kRed
+
+
+def createCanvasPads():
+    ROOT.gStyle.SetOptStat(0)
+    c = TCanvas("c", "canvas", 800, 800)
+    # Upper histogram plot is pad1
+    pad1 = TPad("pad1", "pad1", 0, 0.3, 1, 1.0)
+    pad1.SetBottomMargin(0)  # joins upper and lower plot
+    # pad1.SetGridx()
+    pad1.SetLogy()
+    pad1.Draw()
+    # Lower ratio plot is pad2
+    c.cd()  # returns to main canvas before defining pad2
+    pad2 = TPad("pad2", "pad2", 0, 0, 1, 0.3)
+    pad2.SetTopMargin(0)  # joins upper and lower plot
+    pad2.SetBottomMargin(0.35)
+    pad2.SetGridy()
+    pad2.Draw()
+
+    return c, pad1, pad2
+
+def createRatio(h1, h2):
+    h3 = h1.Clone("h3")
+    h3.SetLineColor(kRed)
+    h3.SetLineWidth(2)
+    h3.SetMarkerStyle(21)
+    h3.SetTitle("")
+    h3.SetMinimum(0)
+    h3.SetMaximum(2)
+    # Set up plot for markers and errors
+    h3.Sumw2()
+    h3.SetStats(0)
+    h3.Divide(h2)
+
+    # Adjust y-axis settings
+    y = h3.GetYaxis()
+    y.SetTitle("Extrapolate / MC")
+    y.SetNdivisions(505)
+    y.SetTitleSize(25)
+    y.SetTitleFont(43)
+    y.SetTitleOffset(1.55)
+    y.SetLabelFont(43)
+    y.SetLabelSize(20)
+
+    # Adjust x-axis settings
+    x = h3.GetXaxis()
+    x.SetTitle("M_{#gamma#gamma} [GeV]")
+    x.SetTitleSize(35)
+    x.SetTitleFont(43)
+    x.SetTitleOffset(0.9)
+    x.SetLabelFont(43)
+    x.SetLabelSize(20)
+
+    return h3
+
+if __name__ == '__main__':
+    f_ext = ROOT.TFile("ResonanceShapes_GluGluSpin0ToGammaGamma_W_0p014_EBEB_2017_finebinned.root")
+    f_gen = ROOT.TFile("InputShapes_GluGluSpin0ToGammaGamma_W_0p014_EBEB_2017.root")
+
+    h_ext = f_ext.Get("h_gg_650")
+    h_gen = f_gen.Get("h_GluGluSpin0ToGammaGamma_W_0p014_M650_2017_8Gev")
+
+    factor=1
+    h_ext.Scale(factor/h_ext.Integral());
+    h_gen.Scale(factor/h_gen.Integral());
+    h_compare = createRatio(h_ext, h_gen)
+
+    c, pad1, pad2 = createCanvasPads()
+    pad1.cd()
+    h_gen.Draw("HIST")
+    h_gen.GetXaxis().SetRangeUser(550,750)
+    h_gen.GetXaxis().SetTitle("M_{#gamma#gamma} [GeV]")
+    h_gen.GetXaxis().SetTitleSize(0.07)
+    h_gen.GetXaxis().SetTitleOffset(0.8)
+    h_gen.GetYaxis().SetTitle("a.u.")
+    h_gen.GetYaxis().SetTitleSize(0.07)
+    h_gen.GetYaxis().SetTitleOffset(0.8)
+    h_gen.SetLineColor(2)
+    h_gen.SetLineWidth(2)
+    h_gen.SetTitle("ggH 650GeV 0.014% 2017")
+    h_ext.Draw("HISTSame")
+    h_ext.SetLineWidth(2)
+    leg = ROOT.TLegend(0.6,0.7,0.9,0.88)
+    leg.AddEntry(h_gen,"MC Generated (UL)","l")
+    leg.AddEntry(h_ext,"Extrapolation (ReReco)","l")
+    leg.Draw()
+    axis = TGaxis(-5, 20, -5, 220, 20, 220, 510, "")
+    axis.SetLabelFont(43)
+    axis.SetLabelSize(15)
+    axis.Draw()
+    pad2.cd()
+    h_compare.Draw("HIST")
+    h_compare.GetXaxis().SetRangeUser(550,700)
+    h_compare.GetXaxis().SetTitle("M_{#gamma#gamma} [GeV]")
+
+    c.SaveAs("test.png")
