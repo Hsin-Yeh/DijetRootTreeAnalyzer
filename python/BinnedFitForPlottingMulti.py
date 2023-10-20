71d6fe6f516d7a6e2a7d65921e0fcdc3825969c8
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-10-20 10:46:25 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1096,7 +1096,7 @@ if __name__ == '__main__':
 
     if options.signalFileName!=None:
         if 'DiPhoton' in box:
-            leg = rt.TLegend(0.58,0.55,0.87,0.87)
+            leg = rt.TLegend(0.58,0.4,1.,0.87)
         else:
             leg = rt.TLegend(0.58,0.55,0.85,0.87)
     else:
