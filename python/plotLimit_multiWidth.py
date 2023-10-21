f0ab35357bb322804b163939ca17db2c2b993bbc
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-10-21 16:55:09 +0800

    update

--- python/plotLimit_multiWidth.py
+++ python/plotLimit_multiWidth.py
@@ -183,6 +183,7 @@ if __name__ == "__main__":
     if ( args.coupling == "kMpl001" ): plabel = "#tilde{k}=0.01,J=2"
     elif ( args.coupling == "kMpl01" ): plabel = "#tilde{k}=0.1,J=2"
     elif ( args.coupling == "kMpl02" ): plabel = "#tilde{k}=0.2,J=2"
+    elif ( args.coupling == "707" ): plabel = "#frac{#Gamma}{m} = 7.1 #times 10^{-3},J=2"
     elif ( args.coupling == "0p014"): plabel = "#frac{#Gamma}{m} = 1.4 #times 10^{-4},J=0"
     elif ( args.coupling == "1p4"): plabel = "#frac{#Gamma}{m} = 1.4 #times 10^{-2},J=0"
     elif ( args.coupling == "5p6"): plabel = "#frac{#Gamma}{m} = 5.6 #times 10^{-2},J=0"
