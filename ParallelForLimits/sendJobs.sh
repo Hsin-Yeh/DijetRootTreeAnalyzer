25bd27e2d0621610b4197709b7f85949d82df617
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-10-23 13:14:16 +0800

    update

--- ParallelForLimits/sendJobs.sh
+++ ParallelForLimits/sendJobs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-version="2023-10-22"
-couplist=(14, 361, 707, 1054, 1400, 2450, 3500, 4550, 5600)
-masslist=($(seq 600 10 5000))
+version="2023-10-23"
+couplist=(4550)
+masslist=(1300)
+# couplist=(14, 361, 707, 1054, 1400, 2450, 3500, 4550, 5600)
+# masslist=($(seq 600 10 5000))
 
 for coupling in "${couplist[@]}"; do
     for mass in "${masslist[@]}"; do
+        jobDir="${version}/${coupling}/${mass}"
         submitFile="${jobDir}/jobs/limit_${coupling}_${mass}.sub"
         #echo ${run}
         chmod 755 ${workpath}/${run}
