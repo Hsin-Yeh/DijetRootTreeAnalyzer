937c4841bde1f64863d963fd0e016059f148c574
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-10-23 14:01:34 +0800

    update

--- ParallelForLimits/createJobs.sh
+++ ParallelForLimits/createJobs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 version="2023-10-23"
+mkdir ${version}
+cd ${version}
+
 # couplist=(4550)
 # masslist=(1300)
 couplist=("14","361","707","1054","1400","2450","3500","4550","5600")
 masslist=($(seq 600 10 5000))
-
-cd ${version}
 
 echo "==========Creating jobs=========="
 for coupling in "${couplist[@]}"; do
