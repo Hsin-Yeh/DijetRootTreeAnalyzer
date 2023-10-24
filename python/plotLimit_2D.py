38230f47d3b7232c63edad66fe43624d604d1f22
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-10-24 10:46:19 +0800

    update

--- python/plotLimit_2D.py
+++ python/plotLimit_2D.py
@@ -35,8 +35,9 @@ def pvalue2D():
             binxy = h_pvalue.GetBin(binx,biny,0)
             with open (in_filename,'r') as infile:
                 Lines = infile.readlines()
-                mass, pvalue = Lines[1].split()
-                h_pvalue.SetBinContent(binxy,float(pvalue))
+                if(len(Lines)>1):
+                    mass, pvalue = Lines[1].split()
+                    h_pvalue.SetBinContent(binxy,float(pvalue))
     c1 = ROOT.TCanvas()
     h_pvalue.Draw("colz")
     c1.SaveAs("test.png")
