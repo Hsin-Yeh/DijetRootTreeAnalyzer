65c8c01d3cd66fd79935e6260078756e4efcac15
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-10-24 11:25:28 +0800

    update

--- python/plotLimit_2D.py
+++ python/plotLimit_2D.py
@@ -31,8 +31,9 @@ def pvalue2D():
     for mass in range(600,5010,10):
         masses.append(mass)
     couplings = array('d',[14, 361, 707, 1054, 1400, 2450, 3500, 4550, 5600])
-    h_zvalue = ROOT.TH2F("h_zvalue","h_zvalue",len(masses)-1,masses,len(couplings)-1,couplings)
-    h_pvalue = ROOT.TH2F("h_pvalue","h_pvalue",len(masses)-1,masses,len(couplings)-1,couplings)
+    couplings = couplings*1e-3
+    h_zvalue = ROOT.TH2F("h_zvalue","zvalue",len(masses)-1,masses,len(couplings)-1,couplings)
+    h_pvalue = ROOT.TH2F("h_pvalue","pvalue",len(masses)-1,masses,len(couplings)-1,couplings)
 
     for coupling in couplings:
         print(coupling)
