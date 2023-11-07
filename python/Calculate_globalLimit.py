a19086ee7df3a24752afc0a5510870e503eb4bf5
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-11-07 11:36:02 +0800

    update

--- python/Calculate_globalLimit.py
+++ python/Calculate_globalLimit.py
@@ -60,8 +60,8 @@ def globalLimit():
                         if (len(Lines)==4 and len(Lines[1].split())==2):
                             toy_zvalue = Lines[3].split()[itoy]
                             toy_zvalues.append(toy_zvalue)
-                except IOError:
-                    print("")
+                # except IOError:
+                #     print("")
 
             sigma_max = max(toy_zvalues)
             for mass, zvalue in zvalues.items():
