b28693948f9b891068a55454b97b6affe1826fc4
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-11-07 13:13:02 +0800

    update

--- python/Calculate_globalLimit.py
+++ python/Calculate_globalLimit.py
@@ -66,7 +66,7 @@ def globalLimit():
                     print("No limits for %i %i"%(coupname,mass))
 
             sigma_max = max(toy_zvalues)
-            print(coupname, len(toy_zvalues), masses[toy_zvalues.index(max(toy_zvalues))], sigma_max)
+            # print(coupname, len(toy_zvalues), masses[toy_zvalues.index(max(toy_zvalues))], sigma_max)
             for mass, zvalue in zvalues.items():
                 if (sigma_max > zvalue):
                     counts[mass] = counts[mass]+1
