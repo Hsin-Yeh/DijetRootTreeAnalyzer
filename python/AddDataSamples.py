e56c2997dea76e39d4c107ca4990b4fd76b5f404
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2023-12-05 13:50:06 +0800

    update

--- python/AddDataSamples.py
+++ python/AddDataSamples.py
@@ -35,18 +35,28 @@ if __name__ == '__main__':
     if options.cat == "EBEB" : cat = 0
     elif options.cat == "EBEE" : cat = 1
 
+    acc={}
+    eff={}
+    acc["EBEB"] = "(mgg > 500 && deltaR > 0.45 && ph1pt>125 && ph2pt>125 && isEBEB)"
+    acc["EBEE"] = "(mgg > 500 && deltaR > 0.45 && ph1pt>125 && ph2pt>125 && (isEBEE || isEEEB))"
+    eff["2016"] = "isGood*(HLT_DoublePhoton60 || HLT_ECALHT800)"
+    eff["2017"] = "isGood*(HLT_DoublePhoton70 || HLT_ECALHT800)"
+    eff["2018"] = "isGood*(HLT_DoublePhoton70 || HLT_ECALHT800)"
+
     for f in args:
         title =  f.split('.root')[0].split('/')[-1].split('_')[0]    
         year = f.split('.root')[0].split('_')[-1]
         print(title,year)
         tfileIn = rt.TFile.Open(f)
-        
+
         h_mgg_1GeVbin = rt.TH1D('h_mgg_1GeVbin','h_mgg_1GeVbin',14000,0,14000)
         # h_mgg_50GeVbin = rt.TH1D('h_mgg_50GeVbin','h_mgg_50GeVbin',160,500,8500)
         # h_mgg_100GeVbin = rt.TH1D('h_mgg_100GeVbin','h_mgg_100GeVbin',80,500,8500)
         thetree=tfileIn.Get("HighMassDiphoton")
 
-        project(thetree,h_mgg_1GeVbin, "mgg", 'eventClass==%d && mgg > 500'%(cat))
+        nomCut = '(' + acc[options.cat] + ')*(' + eff[year] + ')'
+        project(thetree,h_mgg_1GeVbin, "mgg", nomCut)
+        # project(thetree,h_mgg_1GeVbin, "mgg", 'eventClass==%d && mgg > 500'%(cat))
         # project(thetree,h_mgg_50GeVbin, "mgg", 'eventClass==%d && mgg > 500'%(cat))
         # project(thetree,h_mgg_100GeVbin, "mgg", 'eventClass==%d && mgg > 500'%(cat))
       
