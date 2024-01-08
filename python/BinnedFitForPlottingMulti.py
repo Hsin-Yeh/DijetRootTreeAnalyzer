523fd178515211894175bdceeb27d2bd831a65d5
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-08 12:07:32 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -854,7 +854,6 @@ if __name__ == '__main__':
     for key, value in background_pdfs.iteritems():
         if "dijet" == key:
             backgrounds[key]= value.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('p0_%s'%box), w.var('p1_%s'%box), w.var('p2_%s'%box)))
-            print(type(backgrounds[key]))
         if "expow1" == key:
             backgrounds[key]= value.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('pex1_0_%s'%box), w.var('pex1_1_%s'%box), w.var('pex1_2_%s'%box)))
         if "invpow1" == key:
@@ -882,8 +881,16 @@ if __name__ == '__main__':
             #print(background)
 
         backgrounds[key].SetParameter(0,p0_b)
-
-    g_data = rt.TGraphAsymmErrors(myRebinnedTH1)
+    c_fr = rt.TCanvas()
+    backgrounds["dijet"].SetLineColor(rt.kBlack)
+    backgrounds["dijet"].Draw("Al")
+    backgrounds["expow1"].SetLineColor(rt.kRed)
+    backgrounds["expow1"].Draw("lsame")
+    backgrounds["invpow1"].SetLineColor(rt.kGreen)
+    backgrounds["invpow1"].Draw("lsame")
+    backgrounds["invpowlin1"].SetLineColor(rt.kBlue)
+    backgrounds["invpowlin1"].Draw("lsame")
+    c_fr.SaveAs("fit_results.png")
 
     alpha = 1-0.6827
     for i in range(0,g_data.GetN()):
