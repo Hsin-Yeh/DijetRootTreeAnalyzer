fcc5644a88a6996327b3c1a62d2d2f517963d6df
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-10 14:50:52 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -854,16 +854,16 @@ if __name__ == '__main__':
     for key, value in background_pdfs.iteritems():
         if "dijet" == key:
             backgrounds[key]= value.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('p0_%s'%box), w.var('p1_%s'%box), w.var('p2_%s'%box)))
-            print(w.var('Ntot_%s_bkg'%(box)).getVal(), w.var('p0_%s'%box).getVal(), w.var('p1_%s'%box).getVal(), w.var('p2_%s'%box).getVal())
+            # print(w.var('Ntot_%s_bkg'%(box)).getVal(), w.var('p0_%s'%box).getVal(), w.var('p1_%s'%box).getVal(), w.var('p2_%s'%box).getVal())
         if "expow1" == key:
             backgrounds[key]= value.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('pex1_0_%s'%box), w.var('pex1_1_%s'%box), w.var('pex1_2_%s'%box)))
-            print(w.var('Ntot_%s_bkg%s'%(box,key)).getVal(), w.var('pex1_0_%s'%box).getVal(), w.var('pex1_1_%s'%box).getVal(), w.var('pex1_2_%s'%box).getVal())
+            # print(w.var('Ntot_%s_bkg%s'%(box,key)).getVal(), w.var('pex1_0_%s'%box).getVal(), w.var('pex1_1_%s'%box).getVal(), w.var('pex1_2_%s'%box).getVal())
         if "invpow1" == key:
             backgrounds[key]= value.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('pip1_0_%s'%box), w.var('pip1_1_%s'%box), w.var('pip1_2_%s'%box)))
-            print(w.var('Ntot_%s_bkg%s'%(box,key)).getVal(), w.var('pip1_0_%s'%box).getVal(), w.var('pip1_1_%s'%box).getVal(), w.var('pip1_2_%s'%box),getVal())
+            # print(w.var('Ntot_%s_bkg%s'%(box,key)).getVal(), w.var('pip1_0_%s'%box).getVal(), w.var('pip1_1_%s'%box).getVal(), w.var('pip1_2_%s'%box),getVal())
         if "invpowlin1" == key:
             backgrounds[key]= value.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('pil1_0_%s'%box), w.var('pil1_1_%s'%box), w.var('pil1_2_%s'%box), w.var('pil1_3_%s'%box)))
-            print(w.var('Ntot_%s_bkg%s'%(box,key)).getVal(), w.var('pil1_0_%s'%box).getVal(), w.var('pil1_1_%s'%box).getVal(), w.var('pil1_2_%s'%box).getVal(), w.var('pil1_3_%s'%box).getVal())
+            # print(w.var('Ntot_%s_bkg%s'%(box,key)).getVal(), w.var('pil1_0_%s'%box).getVal(), w.var('pil1_1_%s'%box).getVal(), w.var('pil1_2_%s'%box).getVal(), w.var('pil1_3_%s'%box).getVal())
         #background= background_pdf.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('p0_%s'%box), w.var('p1_%s'%box), w.var('p2_%s'%box), w.var('p3_%s'%box), w.var('sqrts') ))
         #background= background_pdf.asTF(rt.RooArgList(w.var('mgg')),rt.RooArgList(w.var('p0_%s'%box) ))
         print("DO I REACH HERE?")
