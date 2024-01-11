ec31d04da6ea822b02e3518006626d0f6b90f6ce
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-11 15:36:32 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
     # extraText.SetTextSize(0.04);
     # extraText.Draw();
 
-    lumiText=ROOT.TLatex(0.73,0.90, "%d fb^{-1} (13 TeV)"%(lumi(args.year)) );
+    lumiText=ROOT.TLatex(0.72,0.90, "%d fb^{-1} (13 TeV)"%(lumi(args.year)) );
     # lumiText=ROOT.TLatex(0.70,0.90, "%d fb^{-1} (13 TeV)"%(138));
     lumiText.SetNDC(1);
     lumiText.SetTextFont(42);
