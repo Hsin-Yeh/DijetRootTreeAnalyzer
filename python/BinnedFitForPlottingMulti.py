80fa306225f16b8093ce9ccb26d673180beaf41e
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-11 13:44:08 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1106,7 +1106,7 @@ if __name__ == '__main__':
             leg = rt.TLegend(0.58,0.55,0.85,0.87)
     else:
         # leg = rt.TLegend(0.6,0.6,0.89,0.89)
-        leg = rt.TLegend(0.56,0.45,0.88,0.89)
+        leg = rt.TLegend(0.56,0.45,0.88,1)
     leg.SetTextFont(42)
     leg.SetTextSize(0.06)
     leg.SetFillColor(rt.kWhite)
