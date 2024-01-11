b7bbb850539a312cc8b8bc692cdd12209f57d1c9
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-11 16:46:35 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1094,7 +1094,7 @@ if __name__ == '__main__':
     #l.DrawLatex(0.3,0.96,"Preliminary")
     # paper
     l.SetTextFont(62)
-    l.SetTextSize(0.055)
+    l.SetTextSize(0.05)
     l.DrawLatex(0.22,0.85,"CMS Supplementary")
     l.SetTextFont(52)
     l.SetTextSize(0.045)
