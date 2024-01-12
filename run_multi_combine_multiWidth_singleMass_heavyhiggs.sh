c9ebb166ce8cc59ed718008c0f11ed240ebd1e97
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-13 03:36:02 +0800

    update

--- run_multi_combine_multiWidth_singleMass_heavyhiggs.sh
+++ run_multi_combine_multiWidth_singleMass_heavyhiggs.sh
@@ -50,20 +50,20 @@ for year in "${yearlist[@]}"; do
 done
 
 # Systematics
-# echo "########## Get Interpolated Systematic Shapes... ##########"
-# for year in "${yearlist[@]}"; do
-#     for cat in "${catlist[@]}"; do
-#         for systematics in {"energyScaleStatUp","energyScaleSystUp","energyScaleGainUp","energySigmaUp","energyScaleStatDown","energyScaleSystDown","energyScaleGainDown","energySigmaDown","SFScaleUp","SFScaleDown","PUScaleUp","PUScaleDown"}; do
-#             echo ${year} ${cat} ${systematics}
-#             narrow_file="${InterpolateShapePath}/inputs/ResonanceShapes_InputShapes_${signal_LongName}_0p014_${cat}_${year}_${systematics}_ratio.root"
-#             medium_file="${InterpolateShapePath}/inputs/ResonanceShapes_InputShapes_${signal_LongName}_1p4_${cat}_${year}_${systematics}_ratio.root"
-#             wide_file="${InterpolateShapePath}/inputs/ResonanceShapes_InputShapes_${signal_LongName}_5p6_${cat}_${year}_${systematics}_ratio.root"
-#             filename="InputShapes_${signal_LongName}_${cat}_${year}_${systematics}"
-#             python ${DijetShapeInterpolator}/extractShapes_width.py -n ${narrow_file} -m ${medium_file} -w ${wide_file} --mass ${mass} > signal_shapes/${filename}_${mass}GeV.py
-#             python ${DijetShapeInterpolator}/getResonanceShapes_width.py -i signal_shapes/${filename}_${mass}GeV.py -m ${mass} -f gg -o signal_shapes/width_${filename}_${mass}GeV.root
-#         done
-#     done
-# done
+echo "########## Get Interpolated Systematic Shapes... ##########"
+for year in "${yearlist[@]}"; do
+    for cat in "${catlist[@]}"; do
+        for systematics in {"energyScaleStatUp","energyScaleSystUp","energyScaleGainUp","energySigmaUp","energyScaleStatDown","energyScaleSystDown","energyScaleGainDown","energySigmaDown","SFScaleUp","SFScaleDown","PUScaleUp","PUScaleDown"}; do
+            echo ${year} ${cat} ${systematics}
+            narrow_file="${InterpolateShapePath}/inputs/ResonanceShapes_InputShapes_${signal_LongName}_0p014_${cat}_${year}_${systematics}_ratio.root"
+            medium_file="${InterpolateShapePath}/inputs/ResonanceShapes_InputShapes_${signal_LongName}_1p4_${cat}_${year}_${systematics}_ratio.root"
+            wide_file="${InterpolateShapePath}/inputs/ResonanceShapes_InputShapes_${signal_LongName}_5p6_${cat}_${year}_${systematics}_ratio.root"
+            filename="InputShapes_${signal_LongName}_${cat}_${year}_${systematics}"
+            python ${DijetShapeInterpolator}/extractShapes_width.py -n ${narrow_file} -m ${medium_file} -w ${wide_file} --mass ${mass} > signal_shapes/${filename}_${mass}GeV.py
+            python ${DijetShapeInterpolator}/getResonanceShapes_width.py -i signal_shapes/${filename}_${mass}GeV.py -m ${mass} -f gg -o signal_shapes/width_${filename}_${mass}GeV.root
+        done
+    done
+done
 # Merge
 python ${DijetShapeInterpolator}/Merge_width_interpolation.py --mass ${mass} --width ${coupling} --signame ${signame}
 
@@ -88,15 +88,16 @@ for year in "${yearlist[@]}"; do
         export datacard_configfile="${DijetRootTreeAnalyzer}/config/diphotons_bias_${year}_pdf_index_wopip_wopil_multiWidth.config"
         python ${DijetRootTreeAnalyzer}/python/WriteDataCard.py --multi -m gg --mass ${mass} ${inputDataDir}/InputShapes_data_${cat}_${year}.root \
             -i ${bkgFitResultsPath}/FitResults_${box}.root --lumi ${lumi} -c ${datacard_configfile} -b ${box} --year ${year} \
-            --SigNorm ${SignalNormFile} signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}.root;
-            # --eneScStatUp    signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleStatUp.root   \
-            # --eneScStatDown  signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleStatDown.root \
-            # --eneScSystUp    signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleSystUp.root   \
-            # --eneScSystDown  signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleSystDown.root \
-            # --eneScGainUp    signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleGainUp.root   \
-            # --eneScGainDown  signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleGainDown.root \
-            # --eneScSigmaUp   signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energySigmaUp.root       \
-            # --eneScSigmaDown signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energySigmaDown.root     \
+            --SigNorm ${SignalNormFile} \
+            --eneScStatUp    signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleStatUp.root   \
+            --eneScStatDown  signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleStatDown.root \
+            --eneScSystUp    signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleSystUp.root   \
+            --eneScSystDown  signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleSystDown.root \
+            --eneScGainUp    signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleGainUp.root   \
+            --eneScGainDown  signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energyScaleGainDown.root \
+            --eneScSigmaUp   signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energySigmaUp.root       \
+            --eneScSigmaDown signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_energySigmaDown.root     \
+            signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}.root;
             # --SFScaleUp      signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_SFScaleUp.root           \
             # --SFScaleDown    signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_SFScaleDown.root         \
             # --PUScaleUp      signal_shapes/ResonanceShapes_InputShapes_${signal_LongName}_${coupling}_${cat}_${year}_PUScaleUp.root           \
