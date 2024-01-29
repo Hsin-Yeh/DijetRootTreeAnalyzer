22f3a5ddb2379422f058482c297343a0a0f209d9
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-29 13:30:22 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1235,22 +1235,22 @@ if __name__ == '__main__':
                           list_chi2AndNdf_background[4], list_chi2AndNdf_background[5],
                           list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]))
 
-    EBText=rt.TLatex(0.2,0.25, "%s"%(options.cat));
+    EBText=rt.TLatex(0.2,0.15, "%s"%(options.cat));
     EBText.SetNDC(1);
-    EBText.SetTextFont(61);
+    EBText.SetTextFont(42);
     EBText.SetLineColor(0);
     EBText.SetLineStyle(1);
     EBText.SetLineWidth(1);
     EBText.SetTextSize(0.06);
     EBText.Draw()
 
-    chiText=rt.TLatex(0.2,0.15, "#chi^{{2}} / NDF = {0:.1f} / {1:d} = {2:.1f}".format(list_chi2AndNdf_background[4], list_chi2AndNdf_background[5], list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]));
+    chiText=rt.TLatex(0.2,0.1, "#chi^{{2}} / NDF = {0:.1f} / {1:d} = {2:.1f}".format(list_chi2AndNdf_background[4], list_chi2AndNdf_background[5], list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]));
     chiText.SetNDC(1);
-    chiText.SetTextFont(61);
+    chiText.SetTextFont(42);
     chiText.SetLineColor(0);
     chiText.SetLineStyle(1);
     chiText.SetLineWidth(1);
-    chiText.SetTextSize(0.04);
+    chiText.SetTextSize(0.05);
     chiText.Draw()
 
     #if 'Calo' in box:
