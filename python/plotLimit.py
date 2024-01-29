11836276acc3e16a55fb7902539a2a0560937bff
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-29 12:44:31 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
     exp2SGraph.GetYaxis().SetTitle("95% CL limit #sigma(pp#rightarrowG#rightarrow#gamma#gamma) (fb)" if args.signame == "grav" else "95% CL limit #sigma(pp#rightarrowS#rightarrow#gamma#gamma) (fb)" );
     exp2SGraph.GetXaxis().SetTitle("m_{G} (GeV)" if args.signame == "grav" else "m_{S} (GeV)");
     exp2SGraph.GetXaxis().SetTitleOffset(1.1);
-    exp2SGraph.GetXaxis().SetLabelSize(0.1);
+    exp2SGraph.GetXaxis().SetLabelSize(0.07);
     exp2SGraph.GetXaxis().SetMoreLogLabels();
     if (args.coupling=="kMpl01" or args.coupling=="kMpl02"): exp2SGraph.GetXaxis().SetRangeUser(600,7000)
     else: exp2SGraph.GetXaxis().SetRangeUser(600,5000)
@@ -184,7 +184,7 @@ if __name__ == "__main__":
     # gr_2016.SetMarkerColor(9);
     # gr_2016.Draw("Psame");
 
-    leg = ROOT.TLegend(0.55,0.5,0.85,0.87,"brNDC");
+    leg = ROOT.TLegend(0.55,0.47,0.85,0.87,"brNDC");
     leg.SetBorderSize(1);
     leg.SetTextFont(62);
     leg.SetLineColor(0);
