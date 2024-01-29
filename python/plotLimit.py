3088d81c4630db9110b770fcb91e8312296a1299
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-01-29 13:22:01 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
     leg.AddEntry(obsGraph,"observed Limit","LP");
     leg.Draw();
 
-    cmsText=ROOT.TLatex(0.18,0.82, "CMS");
+    cmsText=ROOT.TLatex(0.19,0.82, "CMS");
     cmsText.SetNDC(1);
     cmsText.SetTextFont(61);
     cmsText.SetLineColor(0);
