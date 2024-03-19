2b155b1a7a88f47ab0b729f4cf955587142c29e3
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-03-19 13:21:32 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1061,16 +1061,20 @@ if __name__ == '__main__':
 
     if options.doTriggerFit or options.doSimultaneousFit or options.doSpectrumFit or options.noFit:
         rt.gStyle.SetPalette(90)
-        ci = rt.TColor.GetColor("#954a2c");
+        # ci = rt.TColor.GetColor("#954a2c");
+        ci = rt.TColor.GetColor("#e5ab17"); # Solar
         backgrounds["expow1"].SetLineColor(ci)
         backgrounds["expow1"].Draw("csame")
-        ci = rt.TColor.GetColor("#a0333f");
+        # ci = rt.TColor.GetColor("#a0333f");
+        ci = rt.TColor.GetColor("#c55f0f"); # Solar
         backgrounds["invpow1"].SetLineColor(ci)
         backgrounds["invpow1"].Draw("csame")
-        ci = rt.TColor.GetColor("#b006b0");
+        # ci = rt.TColor.GetColor("#b006b0");
+        ci = rt.TColor.GetColor("#a71808"); # Solar
         backgrounds["invpowlin1"].SetLineColor(ci)
         backgrounds["invpowlin1"].Draw("csame")
-        ci = rt.TColor.GetColor("#abec3b");
+        # ci = rt.TColor.GetColor("#abec3b"); # Neon
+        ci = rt.TColor.GetColor("#630005"); # Solar
         backgrounds["dijet"].SetLineColor(ci)
         backgrounds["dijet"].Draw("csame")
     else:
