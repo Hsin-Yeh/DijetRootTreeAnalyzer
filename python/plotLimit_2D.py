2e59b59ffa8bd696ac73f1a0520a196ffed15069
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-03-22 01:51:41 +0800

    update

--- python/plotLimit_2D.py
+++ python/plotLimit_2D.py
@@ -244,13 +244,13 @@ def pvalue2D():
     cmsText.SetLineWidth(1);
     cmsText.SetTextSize(0.04);
 
-    extraText=ROOT.TLatex(0.23,0.90, "Preliminary");
+    extraText=ROOT.TLatex(0.22,0.90, "Preliminary");
     extraText.SetNDC(1);
     extraText.SetTextFont(52);
     extraText.SetLineColor(0);
     extraText.SetLineStyle(1);
     extraText.SetLineWidth(1);
-    extraText.SetTextSize(0.04);
+    extraText.SetTextSize(0.035);
     extraText.Draw();
 
     supText=ROOT.TLatex(0.14,0.90, "CMS Supplementary");
