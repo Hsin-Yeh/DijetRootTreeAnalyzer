03747f74cc16a007241d8d49cd089360c1daa103
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-03-25 21:38:26 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1599,11 +1599,11 @@ if __name__ == '__main__':
 
     if not options.linearX:
         if (options.PAS):
+            c.Print(options.outDir+"/fit_mgg_%s_%s_%s_PAS.pdf"%(fitRegion.replace(',','_'),box,options.year))
+        else:
             c.Print(options.outDir+"/fit_mgg_%s_%s_%s.pdf"%(fitRegion.replace(',','_'),box,options.year))
             c.Print(options.outDir+"/fit_mgg_%s_%s_%s.png"%(fitRegion.replace(',','_'),box,options.year))
             c.Print(options.outDir+"/fit_mgg_%s_%s_%s.C"%(fitRegion.replace(',','_'),box,options.year))
-        else:
-            c.Print(options.outDir+"/fit_mgg_%s_%s_%s_PAS.pdf"%(fitRegion.replace(',','_'),box,options.year))
     else:
         c.Print(options.outDir+"/fit_mgg_%s_%s_linearX.pdf"%(fitRegion.replace(',','_'),box))
         c.Print(options.outDir+"/fit_mgg_%s_%s_linearX.C"%(fitRegion.replace(',','_'),box))
