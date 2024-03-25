301612b1b48484bf4bc0b2dd23d27a5b74b1dcec
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-03-25 21:36:07 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1598,9 +1598,12 @@ if __name__ == '__main__':
     #c.RedrawAxis() # request from David
 
     if not options.linearX:
-        c.Print(options.outDir+"/fit_mgg_%s_%s_%s.pdf"%(fitRegion.replace(',','_'),box,options.year))
-        c.Print(options.outDir+"/fit_mgg_%s_%s_%s.png"%(fitRegion.replace(',','_'),box,options.year))
-        c.Print(options.outDir+"/fit_mgg_%s_%s_%s.C"%(fitRegion.replace(',','_'),box,options.year))
+        if (options.PAS):
+            c.Print(options.outDir+"/fit_mgg_%s_%s_%s.pdf"%(fitRegion.replace(',','_'),box,options.year))
+            c.Print(options.outDir+"/fit_mgg_%s_%s_%s.png"%(fitRegion.replace(',','_'),box,options.year))
+            c.Print(options.outDir+"/fit_mgg_%s_%s_%s.C"%(fitRegion.replace(',','_'),box,options.year))
+        else:
+            c.Print(options.outDir+"/fit_mgg_%s_%s_%s_PAS.pdf"%(fitRegion.replace(',','_'),box,options.year))
     else:
         c.Print(options.outDir+"/fit_mgg_%s_%s_linearX.pdf"%(fitRegion.replace(',','_'),box))
         c.Print(options.outDir+"/fit_mgg_%s_%s_linearX.C"%(fitRegion.replace(',','_'),box))
