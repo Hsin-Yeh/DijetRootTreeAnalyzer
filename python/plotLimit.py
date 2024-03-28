51cf906ad0456f43f6cba1f029459a656414ba81
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-03-29 01:08:20 +0800

    update

--- python/plotLimit.py
+++ python/plotLimit.py
@@ -97,8 +97,9 @@ def main(in_filename):
     for i, line in enumerate(Lines):
         if (signame=="heavyhiggs"):
             if (i%4==0): mass, obs, expP2s, expP1s, exp, expM1s, expM2s = line.split()
-            elif (i%4==1): continue # pvalue=line.split()[1]
-            elif (i%4==2): continue # zvalue=line.split()[1]
+            else: continue;
+            # elif (i%4==1): continue # pvalue=line.split()[1]
+            # elif (i%4==2): continue # zvalue=line.split()[1]
         else:
             mass, obs, expP2s, expP1s, exp, expM1s, expM2s = line.split()
         norm = Acceptance(signame, year, coupling, mass)
