eb875d2cfc5f1e48e8e7327b2fd8682ad2ff30bc
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-23 21:03:44 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -252,9 +252,6 @@ def calculateChi2AndFillResiduals(data_obs_TGraph_,background_hist_,hist_fit_res
 def addSignalHisto(h_bkg):
     sigfile = rt.TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/DijetShapeInterpolator/full/ResonanceShapes_InputShapes_RSGravitonToGammaGamma_kMpl001_EBEB_2016.root")
     h_sig = sigfile.Get("h_gg_1313")
-    ctest = rt.TCanvas()
-    h_sig.Draw("HIST")
-    ctest.SaveAs("test.png")
     # h_sig.Scale(1000)
     # h_sig.Add(h_bkg)
     return h_sig
@@ -1033,7 +1030,7 @@ if __name__ == '__main__':
     # Draw data
     g_data_clone.Draw("zpsame")
     g_data.Draw("zpsame")
-    # h_sig.Draw("HISTsame")
+    h_sig.Draw("HISTsame")
     # Draw fit function
     if options.doTriggerFit or options.doSimultaneousFit or options.doSpectrumFit or options.noFit:
         rt.gStyle.SetPalette(90)
