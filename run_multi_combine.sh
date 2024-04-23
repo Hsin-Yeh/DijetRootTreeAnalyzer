76a50f9580c4193d9caca36b8a50c28b37cf1049
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 01:04:41 +0800

    update

--- run_multi_combine.sh
+++ run_multi_combine.sh
@@ -30,7 +30,7 @@ export coupling=$2
 
 #################### Constants ####################
 # Cats
-export catlist=("EBEB")
+export catlist=("EBEB" "EBEE")
 # Lumi
 export lumi=35.9
 export lumi_1000=35900
