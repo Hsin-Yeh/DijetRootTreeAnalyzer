553d8a8b9e931ff2b346237eea8b8ca3512b2db8
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 00:22:22 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1159,7 +1159,7 @@ if __name__ == '__main__':
         l.SetTextSize(0.05)
         l.DrawLatex(0.22,0.85,"CMS Supplementary")
 
-    leg = rt.TLegend(0.63,0.38,0.88,0.88)
+    leg = rt.TLegend(0.6,0.38,0.88,0.88)
     leg.SetTextFont(42)
     leg.SetTextSize(0.06)
     leg.SetFillColor(rt.kWhite)
@@ -1180,15 +1180,15 @@ if __name__ == '__main__':
     leg.AddEntry(backgrounds["invpow1"],"%s"%(modelforms["invpow1"]),"l")
     leg.AddEntry(backgrounds["invpowlin1"],"%s"%(modelforms["invpowlin1"]),"l")
 
-    legsig = rt.TLegend(0.35,0.75,0.58,0.89)
+    legsig = rt.TLegend(0.35,0.73,0.58,0.88)
     legsig.SetTextFont(42)
     legsig.SetTextSize(0.05)
     legsig.SetFillColor(rt.kWhite)
     legsig.SetFillStyle(0)
     legsig.SetLineWidth(0)
     legsig.SetLineColor(rt.kWhite)
-    legsig.AddEntry(sighist_600,"#tilde{k}=0.01,0.6TeV","f")
-    legsig.AddEntry(sighist_1000,"#tilde{k}=0.01,1TeV","f")
+    legsig.AddEntry(sighist_600,"#tilde{k}=0.01, 0.6TeV","f")
+    legsig.AddEntry(sighist_1000,"#tilde{k}=0.01, 1TeV","f")
 
     # for key, value in backgrounds.iteritems():
         # leg.AddEntry(value,"%s: %s "%(key, modelforms[key]),"l")
