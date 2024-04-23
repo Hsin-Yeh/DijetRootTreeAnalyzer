173f80a12f79f248c9a525e1ef15bc495897dda7
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 00:48:38 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1180,7 +1180,7 @@ if __name__ == '__main__':
     leg.AddEntry(backgrounds["invpow1"],"%s"%(modelforms["invpow1"]),"l")
     leg.AddEntry(backgrounds["invpowlin1"],"%s"%(modelforms["invpowlin1"]),"l")
 
-    legsig = rt.TLegend(0.23,0.75,0.5,0.9)
+    legsig = rt.TLegend(0.23,0.73,0.5,0.88)
     legsig.SetTextFont(42)
     legsig.SetTextSize(0.05)
     legsig.SetFillColor(rt.kWhite)
