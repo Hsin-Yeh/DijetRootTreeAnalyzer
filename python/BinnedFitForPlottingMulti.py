dde136d7d179d0d05e820e05fa994aa864f97a91
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-23 21:12:51 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -253,9 +253,9 @@ def addSignalHisto(h_bkg):
     sigfile = rt.TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/DijetShapeInterpolator/full/ResonanceShapes_InputShapes_RSGravitonToGammaGamma_kMpl001_EBEB_2016.root")
     h_sig = sigfile.Get("h_gg_1313")
     # h_sig.Scale(1000)
-    h_sig.Add(h_bkg)
+    # h_sig.Add(h_bkg)
     ctest = rt.TCanvas()
-    h_sig.Draw("HIST")
+    h_bkg.Draw("HIST")
     ctest.SaveAs("test.png")
     return h_sig
 
