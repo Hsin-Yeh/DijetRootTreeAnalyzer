25838a44e801995c370556c26e5bf9b906f5da8f
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 04:34:13 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1223,7 +1223,7 @@ if __name__ == '__main__':
         l.SetTextSize(0.05)
         l.DrawLatex(0.22,0.85,"CMS Supplementary")
 
-    leg = rt.TLegend(0.2,0.12,0.49,0.54)
+    leg = rt.TLegend(0.2,0.12,0.52,0.54)
     # leg = rt.TLegend(0.6,0.4,0.88,0.88)
     leg.SetTextFont(42)
     leg.SetTextSize(0.058)
@@ -1323,7 +1323,7 @@ if __name__ == '__main__':
                           list_chi2AndNdf_background[4], list_chi2AndNdf_background[5],
                           list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]))
 
-    EBText=rt.TLatex(0.4,0.83, "%s"%(options.cat));
+    EBText=rt.TLatex(0.39,0.83, "%s"%(options.cat));
     EBText.SetNDC(1);
     EBText.SetTextFont(42);
     EBText.SetLineColor(0);
