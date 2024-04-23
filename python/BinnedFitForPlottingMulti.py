d18cc714ede3d626ace423c1c4f4a7e89638d02e
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 02:49:26 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1204,7 +1204,7 @@ if __name__ == '__main__':
         l.SetTextSize(0.05)
         l.DrawLatex(0.22,0.85,"CMS Supplementary")
 
-    leg = rt.TLegend(0.19,0.15,0.45,0.6)
+    leg = rt.TLegend(0.21,0.05,0.43,0.55)
     # leg = rt.TLegend(0.6,0.4,0.88,0.88)
     leg.SetTextFont(42)
     leg.SetTextSize(0.06)
@@ -1227,7 +1227,7 @@ if __name__ == '__main__':
     leg.AddEntry(backgrounds["invpowlin1"],"%s"%(modelforms["invpowlin1"]),"l")
 
     # legsig = rt.TLegend(0.21,0.15,0.4,0.4)
-    legsig = rt.TLegend(0.4,0.6,0.88,0.88)
+    legsig = rt.TLegend(0.55,0.6,0.88,0.88)
     legsig.SetTextFont(42)
     legsig.SetTextSize(0.045)
     legsig.SetFillColor(rt.kWhite)
@@ -1304,7 +1304,7 @@ if __name__ == '__main__':
                           list_chi2AndNdf_background[4], list_chi2AndNdf_background[5],
                           list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]))
 
-    EBText=rt.TLatex(0.21,0.75, "%s"%(options.cat));
+    EBText=rt.TLatex(0.4,0.85, "%s"%(options.cat));
     EBText.SetNDC(1);
     EBText.SetTextFont(42);
     EBText.SetLineColor(0);
@@ -1313,13 +1313,13 @@ if __name__ == '__main__':
     EBText.SetTextSize(0.062);
     EBText.Draw()
 
-    chiText=rt.TLatex(0.21,0.05, "#chi^{{2}} / dof = {0:.1f} / {1:d} = {2:.1f}".format(list_chi2AndNdf_background[4], list_chi2AndNdf_background[5], list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]));
+    chiText=rt.TLatex(0.21,0.75, "#chi^{{2}} / dof = {0:.1f} / {1:d} = {2:.1f}".format(list_chi2AndNdf_background[4], list_chi2AndNdf_background[5], list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]));
     chiText.SetNDC(1);
     chiText.SetTextFont(42);
     chiText.SetLineColor(0);
     chiText.SetLineStyle(1);
     chiText.SetLineWidth(1);
-    if options.year=="fullRun2": chiText.SetTextSize(0.053)
+    if options.year=="fullRun2": chiText.SetTextSize(0.05)
     else: chiText.SetTextSize(0.045);
     chiText.Draw()
 
