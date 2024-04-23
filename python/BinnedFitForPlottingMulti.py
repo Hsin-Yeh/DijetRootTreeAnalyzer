14f20f125ec068cb2474f46b1d1f1978d033e785
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-23 14:33:39 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -250,7 +250,7 @@ def calculateChi2AndFillResiduals(data_obs_TGraph_,background_hist_,hist_fit_res
     return [chi2_FullRangeAll, ndf_FullRangeAll, chi2_PlotRangeAll, ndf_PlotRangeAll, chi2_PlotRangeNonZero, ndf_PlotRangeNonZero, chi2_PlotRangeMinNumEvents, ndf_PlotRangeMinNumEvents]
 
 def addSignalHisto(h_bkg):
-    sigfile = rt.TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/DijetShapeInterpolator/full_backup/ResonanceShapes_InputShapes_RSGravitonToGammaGamma_kMpl001_EBEB_2016.root")
+    sigfile = rt.TFile("/afs/cern.ch/work/h/hsinyeh/public/diphoton-analysis/CMSSW_10_2_13/src/diphoton-analysis/DijetShapeInterpolator/full/ResonanceShapes_InputShapes_RSGravitonToGammaGamma_kMpl001_EBEB_2016.root")
     h_sig = sigfile.Get("h_gg_1313")
     h_sig.Scale(1000)
     h_sig.Add(h_bkg)
