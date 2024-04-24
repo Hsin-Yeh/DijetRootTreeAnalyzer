71b82fe52cdaba95206f9462271f87c1f8995a75
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-25 00:48:09 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1537,7 +1537,7 @@ if __name__ == '__main__':
     axis.SetLineColor(rt.kRed);
     axis.SetLabelColor(rt.kRed);
     axis.Draw("same");
-    # h_residual_1320.Draw("HISTSame")
+    h_residual_1320.Draw("HISTSame")
     # h_residual_2200.Draw("HISTSame")
     # h_fit_residual_vs_mass.Draw("histsame")
 
