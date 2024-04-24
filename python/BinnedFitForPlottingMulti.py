3c4f59a8d94c474e0726f0d37e609759a415615d
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 12:03:46 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1213,7 +1213,7 @@ if __name__ == '__main__':
     l.SetTextFont(62)
     if(options.year=="fullRun2"):
         l.SetTextSize(0.065)
-        l.DrawLatex(0.27,0.83,"CMS")
+        l.DrawLatex(0.21,0.83,"CMS")
         # PAS
         l.SetTextFont(52)
         l.SetTextSize(0.055)
@@ -1244,7 +1244,7 @@ if __name__ == '__main__':
     leg.AddEntry(backgrounds["invpow1"],"%s"%(modelforms["invpow1"]),"l")
     leg.AddEntry(backgrounds["invpowlin1"],"%s"%(modelforms["invpowlin1"]),"l")
 
-    legsig = rt.TLegend(0.21,0.15,0.4,0.4)
+    legsig = rt.TLegend(0.21,0.12,0.4,0.35)
     # legsig = rt.TLegend(0.58,0.68,0.85,0.88)
     legsig.SetTextFont(42)
     legsig.SetTextSize(0.043)
@@ -1322,7 +1322,7 @@ if __name__ == '__main__':
                           list_chi2AndNdf_background[4], list_chi2AndNdf_background[5],
                           list_chi2AndNdf_background[4]/list_chi2AndNdf_background[5]))
 
-    EBText=rt.TLatex(0.39,0.83, "%s"%(options.cat));
+    EBText=rt.TLatex(0.3,0.83, "%s"%(options.cat));
     EBText.SetNDC(1);
     EBText.SetTextFont(42);
     EBText.SetLineColor(0);
