056cfd7323aad303013e07d2cca4d799c9f04186
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 11:26:15 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -257,11 +257,15 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat):
     sighist_1320.SetDirectory(0)
     sighist_2200.SetDirectory(0)
     if(cat=="EBEB"):
-        sighist_1320.Scale(1000*0.00268570521957342*138*0.386773)
-        sighist_2200.Scale(1000*0.00011876885597882948*138*0.483016)
+        factor = 1000*0.00268570521957342*138*0.386773.;
+        sighist_1320.Scale(factor/sighist_1320.Integral(), "width");
+        factor = 1000*0.00011876885597882948*138*0.483016
+        sighist_2200.Scale(factor/sighist_2200.Integral(), "width")
     elif(cat=="EBEE"):
-        sighist_1320.Scale(1000*0.00268570521957342*138*0.190722)
-        sighist_2200.Scale(1000*0.00011876885597882948*138*0.137251)
+        factor = 1000*0.00268570521957342*138*0.190722;
+        sighist_1320.Scale(factor/sighist_1320.Integral(), "width");
+        factor = 1000*0.00011876885597882948*138*0.137251
+        sighist_2200.Scale(factor/sighist_2200.Integral(), "width")
     h_bkg.SetFillColorAlpha(0,0)
     h_bkg.SetLineColorAlpha(0,0)
     ci = rt.TColor.GetColor("#1068da"); # Bird
