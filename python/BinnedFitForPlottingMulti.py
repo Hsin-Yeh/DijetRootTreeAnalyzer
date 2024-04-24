0594a6dc6e87a07e452d55f9d897a8a66de465f3
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-24 08:52:02 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1520,7 +1520,7 @@ if __name__ == '__main__':
     h_residual_2200.Draw("HISTSame")
     h_fit_residual_vs_mass.Draw("histsame")
 
-    legres = rt.TLegend(0.63,0.3,0.9,0.52)
+    legres = rt.TLegend(0.63,0.3,0.9,0.51)
     legres.SetTextFont(42)
     legres.SetTextSize(0.055)
     legres.SetFillColor(rt.kWhite)
