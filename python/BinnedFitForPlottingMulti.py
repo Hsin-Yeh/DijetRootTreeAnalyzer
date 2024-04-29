6349d91d5e29249ba886254151d4328a27dc0be8
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-30 01:16:24 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -280,7 +280,7 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat,lumi):
     if(cat=="EBEB"):
         factor = limit_02_1320*lumi*norm_02_1320_EBEB
         sighist_1320.Scale(factor/sighist_1320.Integral(),"width")
-        factor = limit_001_2200*lumi*norm_001_2200_EBEB
+        factor = limit_02_2200*lumi*norm_02_2200_EBEB
         sighist_2200.Scale(factor/sighist_2200.Integral(), "width")
     elif(cat=="EBEE"):
         factor = xsec_02_1320*lumi*norm_02_1320_EBEE
