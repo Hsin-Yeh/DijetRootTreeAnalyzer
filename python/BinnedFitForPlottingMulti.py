d10d6aacda5feec532f6e755b8904fcff9bdb2a3
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-02 02:03:24 +0800

    upddate

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1536,7 +1536,7 @@ if __name__ == '__main__':
     #h_fit_residual_vs_mass.GetYaxis().SetTitleOffset(0.5)
     #h_fit_residual_vs_mass.GetYaxis().SetTitle('#frac{(Data-Fit)}{#sigma_{Data}}')
     # paper
-    h_fit_residual_vs_mass.GetYaxis().CenterTitle(1);
+    # h_fit_residual_vs_mass.GetYaxis().CenterTitle(1);
     h_fit_residual_vs_mass.GetYaxis().SetTitleOffset(0.5)
     # h_fit_residual_vs_mass.GetYaxis().SetTitle('#frac{Data-Fit}{Uncertainty}')
     h_fit_residual_vs_mass.GetYaxis().SetTitle('Pull')
