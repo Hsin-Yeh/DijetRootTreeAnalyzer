025356b3d36b3cae3796e6012c7bcb72d1385f5a
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-03 11:13:45 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -294,12 +294,12 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat,lumi):
         sighist_2200.Scale(factor/sighist_2200.Integral(), "width")
     print("Integral check: 1320GeV %f and 2200GeV %f"%(sighist_1320.Integral("width"),sighist_2200.Integral("width")))
     h_bkg.SetFillColorAlpha(2,0.4)
-    h_bkg.SetFillStyle(0)
+    # h_bkg.SetFillStyle(0)
     ci = rt.TColor.GetColor("#1068da"); # Bird
-    sighist_1320.SetFillColorAlpha(ci,0.4)
+    sighist_1320.SetFillColorAlpha(ci,0)
     sighist_1320.SetLineColorAlpha(0,0)
     ci = rt.TColor.GetColor("#2cb6a5"); # Bird
-    sighist_2200.SetFillColorAlpha(ci,0.4)
+    sighist_2200.SetFillColorAlpha(ci,0)
     sighist_2200.SetLineColorAlpha(0,0)
     hstack_1320.Add(h_bkg)
     hstack_1320.Add(sighist_1320)
