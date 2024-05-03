febb3dbf5230dbf937166e0a74cd350806f52474
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-03 11:31:39 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -299,8 +299,15 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat,lumi):
     sighist_2200.SetFillColorAlpha(ci,0)
     sighist_2200.SetLineColorAlpha(0,0)
     h_bkg.SetFillColorAlpha(2,0)
+    hempty = h_bkg.Clone()
+    for bin in range (0,h_bkg.GetNBins()):
+        value = 0
+        hempty.SetBinContent(bin+1,0)
+
+    hstack_1320.Add(hempty)
     hstack_1320.Add(h_bkg)
     hstack_1320.Add(sighist_1320)
+    hstack_2200.Add(hempty)
     hstack_2200.Add(h_bkg)
     hstack_2200.Add(sighist_2200)
 
