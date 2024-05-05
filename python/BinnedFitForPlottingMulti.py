754f941786fdc2a597364859390fbac09e7f419e
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-05 21:32:46 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1147,11 +1147,11 @@ if __name__ == '__main__':
     myRebinnedDensityTH1.Draw("axis")
 
     # Draw data
-    hstack_dummy.Draw("same NOCLEAR")
     g_data_clone.Draw("zpsame")
     g_data.Draw("zpsame")
     if (options.year == "fullRun2"):
         # h_bkg.SetFillColorAlpha(0,0)
+        hstack_dummy.Draw("same NOCLEAR")
         hstack_1320.Draw("same NOCLEAR")
         hstack_2200.Draw("same NOCLEAR")
     myRebinnedDensityTH1.Draw("axissame")
