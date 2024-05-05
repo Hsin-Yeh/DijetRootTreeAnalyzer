7bfc41c82a234c532a70f671a518f55f78a99771
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-05 21:41:03 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1151,9 +1151,9 @@ if __name__ == '__main__':
     g_data.Draw("zpsame")
     if (options.year == "fullRun2"):
         # h_bkg.SetFillColorAlpha(0,0)
-        hstack_dummy.Draw("HISTsame NOCLEAR")
-        hstack_1320.Draw("HISTsame")
-        hstack_2200.Draw("HISTsame")
+        # hstack_dummy.Draw("HISTsame NOCLEAR")
+        hstack_1320.Draw("HISTsame NOCLEAR")
+        hstack_2200.Draw("HISTsame NOCLEAR")
     myRebinnedDensityTH1.Draw("axissame")
     # Draw fit function
     if options.doTriggerFit or options.doSimultaneousFit or options.doSpectrumFit or options.noFit:
