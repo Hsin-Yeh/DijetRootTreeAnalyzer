a2e9e3a2312bf3fbae5e74dd27a873f9298394bc
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-05 22:32:38 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -296,16 +296,15 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat,lumi):
         sighist_2200.Scale(factor/sighist_2200.Integral(), "width")
     print("Integral check: 1320GeV %f and 2200GeV %f"%(sighist_1320.Integral("width"),sighist_2200.Integral("width")))
     ci = rt.TColor.GetColor("#1068da"); # Bird
-    sighist_1320.SetFillColorAlpha(ci,0.4)
+    sighist_1320.SetFillColorAlpha(ci,0.8)
     sighist_1320.SetLineColorAlpha(ci,0)
     # sighist_1320.SetFillStyle(4050)
     ci = rt.TColor.GetColor("#2cb6a5"); # Bird
-    sighist_2200.SetFillColorAlpha(ci,0.4)
+    sighist_2200.SetFillColorAlpha(ci,0.8)
     sighist_2200.SetLineColorAlpha(ci,0)
     # sighist_2200.SetFillStyle(4050)
-    h_bkg.SetFillColorAlpha(0,0.4)
+    h_bkg.SetFillColorAlpha(0,0)
     h_bkg.SetLineColorAlpha(0,0)
-    h_bkg.SetFillStyle(3995)
     h_bkg.SetDirectory(0)
     hstack_1320.Add(h_bkg)
     hstack_1320.Add(sighist_1320)
@@ -1149,13 +1148,13 @@ if __name__ == '__main__':
     myRebinnedDensityTH1.Draw("axis")
 
     # Draw data
-    g_data_clone.Draw("zpsame")
-    g_data.Draw("zpsame")
     if (options.year == "fullRun2"):
         # h_bkg.SetFillColorAlpha(0,0)
         # hstack_dummy.Draw("HISTsame NOCLEAR")
-        # hstack_1320.Draw("HISTsame")
+        hstack_1320.Draw("HISTsame")
         hstack_2200.Draw("HISTsame")
+    g_data_clone.Draw("zpsame")
+    g_data.Draw("zpsame")
     myRebinnedDensityTH1.Draw("axissame")
     # Draw fit function
     if options.doTriggerFit or options.doSimultaneousFit or options.doSpectrumFit or options.noFit:
