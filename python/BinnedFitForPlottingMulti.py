ffdcfa7098dad6563e48bdd6c61db6b43ceb259e
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-05 21:26:49 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -309,8 +309,8 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat,lumi):
     hstack_1320.Add(sighist_1320)
     hstack_2200.Add(h_bkg)
     hstack_2200.Add(sighist_2200)
-    hstack_dummy.Add(sighist_2200)
-    hstack_dummy.Add(sighist_1320)
+    hstack_dummy.Add(h_bkg)
+    # hstack_dummy.Add(sighist_1320)
 
     h_residual_1320 = rt.TH1D("h_residual_1320","h_residual_1320",N_massBins_,massBins_)
     ci = rt.TColor.GetColor("#1068da"); # Bird
