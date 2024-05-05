3b0458095393c64ed9745f06f8404a5f550941d7
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-05 21:17:48 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -295,14 +295,14 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat,lumi):
     ci = rt.TColor.GetColor("#1068da"); # Bird
     sighist_1320.SetFillColorAlpha(ci,0.4)
     sighist_1320.SetLineColorAlpha(0,0)
-    sighist_1320.SetFillStyle(3001)
+    sighist_1320.SetFillStyle(4000)
     ci = rt.TColor.GetColor("#2cb6a5"); # Bird
     sighist_2200.SetFillColorAlpha(ci,0.4)
     sighist_2200.SetLineColorAlpha(0,0)
-    sighist_2200.SetFillStyle(3001)
+    sighist_2200.SetFillStyle(4000)
     h_bkg.SetFillColorAlpha(2,0.4)
     h_bkg.SetLineColorAlpha(0,0)
-    h_bkg.SetFillStyle(3001)
+    h_bkg.SetFillStyle(4000)
     h_bkg.SetDirectory(0)
     hstack_1320.Add(h_bkg)
     hstack_1320.Add(sighist_1320)
