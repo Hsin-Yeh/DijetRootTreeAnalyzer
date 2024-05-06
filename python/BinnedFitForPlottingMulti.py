51dddafc7cf1270e6869e8092304c4c54dc621b7
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-06 11:36:48 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -1569,6 +1569,11 @@ if __name__ == '__main__':
     h_fit_residual_vs_mass.GetXaxis().SetTitleSize(2*0.06)
     h_fit_residual_vs_mass.GetXaxis().SetLabelSize(2*0.05)
 
+    h_fit_residual_vs_mass.Draw("histsame")
+    # rightmax = 1.1*h_residual_1320.GetMaximum();
+    # scale = rt.gPad.GetUymax()/rightmax;
+    # h_residual_1320.Scale(scale);
+    # h_residual_2200.Scale(scale);
     if (options.year == "fullRun2"):
         h_residual_1320.Draw("HISTSame")
         h_residual_2200.Draw("HISTSame")
