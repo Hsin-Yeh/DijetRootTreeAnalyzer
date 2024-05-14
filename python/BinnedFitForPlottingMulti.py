6667d342abed9e174f3549371d4d79cfa5424100
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-14 09:48:39 +0800

    update

--- python/BinnedFitForPlottingMulti.py
+++ python/BinnedFitForPlottingMulti.py
@@ -305,7 +305,7 @@ def addSignalHisto(h_bkg,data_obs_TGraph_,N_massBins_,massBins_,cat,lumi):
     # ci = rt.TColor.GetColor('#b16cab')
     # ci = rt.TColor.GetColor('#29b6f6')
     # ci = rt.TColor.GetColor("#FF3333")
-    # ci = rt.TColor.GetColor('#EE4B2B')
+    ci = rt.TColor.GetColor('#EE4B2B')
     # sighist_1320.SetFillColorAlpha(ci,0.8)
     sighist_1320.SetFillColorAlpha(rt.kRed,1)
     sighist_1320.SetLineColorAlpha(ci,0)
